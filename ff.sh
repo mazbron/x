16a35f7ab3a0793eaafcbe432b6957f9e5941e29
Mazbron <mazbronads@gmail.com>
2016-12-11 23:32:39 +0700

    Update ff.sh

--- ff.sh
+++ ff.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 while true
 do
-firefox imacros://run/?m=tek.iim & sleep 3600
+firefox -P 1 -no-remote imacros://run/?m=tek.iim & sleep 2 & firefox -P 2 -no-remote imacros://run/?m=tek.iim & sleep 1000
 killall firefox
 sleep 5
 done
