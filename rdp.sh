66c669f8669b9164880efebd0a19696e212d2296
Mazbron <mazbronads@gmail.com>
2017-01-08 22:57:13 +0700

    Update rdp.sh

--- rdp.sh
+++ rdp.sh
@@ -16,5 +16,23 @@ apt-get install chromium-browser -y
 sleep 1
 apt-get install firefox -y
 sleep 1
+echo download ff profile
+sleep 1
+mv .mozilla .mozilla.bak
+wget http://149.56.111.62/mozilla.tar.gz
+tar -xvf mozilla.tar.gz
+wget https://raw.githubusercontent.com/mazbron/x/master/ff3.sh
+chmod +x ff3.sh
+sleep 1
+echo install chrome
+sleep 1
+wget https://raw.githubusercontent.com/mazbron/x/master/chrome.sh
+chmod +x chrome.sh
+./chrome.sh
+sleep 1
+mkdir Desktop
+cd Desktop
+wget https://raw.githubusercontent.com/mazbron/x/master/ch2.sh
+chmod +x ch2.sh
 echo You have successfully Installed LXDE Desktop Environment.. Enjoy..!!
 echo Ceeeers... MAZBRON.com _ BESTSEOTOOL.co
