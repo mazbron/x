5d0c486f1c152e4bd4b6f8a91304b1cb429eaed7
Mazbron <mazbronads@gmail.com>
2017-01-14 22:28:15 +0700

    Create ch4.sh

---
+++ ch4.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+COUNTER=0
+while :
+do
+/usr/bin/google-chrome-stable --user-data-dir=/home/chrome https://t.co/OtbM2N0lS0 --incognito --disable-session--crashed-bubble & sleep 2 & /usr/bin/google-chrome-stable --user-data-dir=/home/chrome https://t.co/OtbM2N0lS0 --incognito --disable-session--crashed-bubble & sleep 2 & /usr/bin/google-chrome-stable --user-data-dir=/home/chrome https://t.co/OtbM2N0lS0 --incognito --disable-session--crashed-bubble & sleep 2 & /usr/bin/google-chrome-stable --user-data-dir=/home/chrome https://t.co/OtbM2N0lS0 --incognito --disable-session--crashed-bubble & sleep 2 & /usr/bin/google-chrome-stable --user-data-dir=/home/chrome https://t.co/OtbM2N0lS0 --incognito --disable-session--crashed-bubble & sleep 2 & /usr/bin/google-chrome-stable --user-data-dir=/home/chrome https://t.co/OtbM2N0lS0 --incognito --disable-session--crashed-bubble & sleep 500
+killall chrome
+sleep 5
+done
