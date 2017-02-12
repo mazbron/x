db89439a07d4e89d5080fd4071132b329716c98b
Mazbron <mazbronads@gmail.com>
2017-02-12 20:59:02 +0700

    Update ff.sh

--- ff.sh
+++ ff.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 while true
 do
-firefox -P 1 -no-remote imacros://run/?m=tek.iim & sleep 2 & firefox -P 3 -no-remote imacros://run/?m=tek.iim & sleep 2 & firefox -P 4 -no-remote imacros://run/?m=tek.iim & sleep 500
-killall firefox
-sleep 5
-firefox -P 2 -no-remote imacros://run/?m=tek.iim & sleep 2 & firefox -P 4 -no-remote imacros://run/?m=tek.iim & sleep 2 & firefox -P 1 -no-remote imacros://run/?m=tek.iim  & sleep 500
+firefox -P 3 -no-remote imacros://run/?m=85ideas.iim & sleep 10
+firefox -P 4 -no-remote imacros://run/?m=85ideas.iim & sleep 1000
+#killall firefox
+#sleep 5
+#firefox -P 2 -no-remote & sleep 2 & firefox -P 4 -no-remote & sleep 500
 killall firefox
 sleep 5
 done
