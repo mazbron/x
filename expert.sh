dc3772e8b73344960499f86a860d0d7d6b608120
Mazbron <mazbronads@gmail.com>
2017-02-13 05:26:09 +0000

    Add files via upload

---
+++ expert.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+while :
+do
+google-chrome https://t.co/ONr8ChhpAb --no-sandbox --disable-session--crashed-bubble --incognito & sleep 3
+google-chrome https://t.co/ONr8ChhpAb --no-sandbox --disable-session--crashed-bubble --incognito & sleep 3
+google-chrome https://t.co/ONr8ChhpAb --no-sandbox --disable-session--crashed-bubble --incognito & sleep 3
+google-chrome https://t.co/ONr8ChhpAb --no-sandbox --disable-session--crashed-bubble --incognito & sleep 3
+google-chrome https://t.co/ONr8ChhpAb --no-sandbox --disable-session--crashed-bubble --incognito & sleep 3
+google-chrome https://t.co/ONr8ChhpAb --no-sandbox --disable-session--crashed-bubble --incognito & sleep 3
+google-chrome https://t.co/ONr8ChhpAb --no-sandbox --disable-session--crashed-bubble --incognito & sleep 3
+google-chrome https://t.co/ONr8ChhpAb --no-sandbox --disable-session--crashed-bubble --incognito & sleep 3
+google-chrome https://t.co/ONr8ChhpAb --no-sandbox --disable-session--crashed-bubble --incognito & sleep 3
+google-chrome https://t.co/ONr8ChhpAb --no-sandbox --disable-session--crashed-bubble --incognito & sleep 3
+google-chrome https://t.co/ONr8ChhpAb --no-sandbox --disable-session--crashed-bubble --incognito & sleep 1000
+killall chrome
+killall chrome
+sleep 5
+done
