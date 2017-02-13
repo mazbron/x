9632b9cf697b98f30f077700e34655dbb70f376b
Mazbron <mazbronads@gmail.com>
2017-02-13 13:56:45 +0700

    Update ext.sh

--- ext.sh
+++ ext.sh
@@ -1,2 +1,2 @@
-google-chrome --no-sandbox https://chrome.google.com/webstore/detail/ipfuck/bjgmbpodpcgmnpfjmigcckcjfldcicnd?hl=en
+google-chrome --no-sandbox https://chrome.google.com/webstore/detail/ipfuck/bjgmbpodpcgmnpfjmigcckcjfldcicnd?hl=en & sleep 2
 google-chrome --no-sandbox https://chrome.google.com/webstore/detail/random-user-agent/einpaelgookohagofgnnkcfjbkkgepnp?hl=en
