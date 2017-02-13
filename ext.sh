66f7ffa31f177c26284aef126098911b3ec5317c
Mazbron <mazbronads@gmail.com>
2017-02-13 13:14:26 +0700

    Create ext.sh

---
+++ ext.sh
@@ -0,0 +1,2 @@
+google-chrome --no-sandbox https://chrome.google.com/webstore/detail/ipfuck/bjgmbpodpcgmnpfjmigcckcjfldcicnd?hl=en
+google-chrome --no-sandbox https://chrome.google.com/webstore/detail/random-user-agent/einpaelgookohagofgnnkcfjbkkgepnp?hl=en
