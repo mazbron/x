c65cf64c20b9f13c8bab88d0bd9042b63eea9f51
Mazbron <mazbronads@gmail.com>
2017-02-16 13:49:38 +0700

    Create nicks.sh

---
+++ nicks.sh
@@ -0,0 +1,18 @@
+!/bin/bash
+while :
+do
+google-chrome --app="data:text/html,<html><body><script>window.moveTo(1,1);window.resizeTo(947,592);window.location='https://t.co/kMzfpsXRGl';</script></body></html>" --no-sandbox --disable-session--crashed-bubble --incognito & sleep 3
+google-chrome --app="data:text/html,<html><body><script>window.moveTo(20,20);window.resizeTo(947,592);window.location='https://t.co/kMzfpsXRGl';</script></body></html>" --no-sandbox --disable-session--crashed-bubble --incognito & sleep 3
+google-chrome --app="data:text/html,<html><body><script>window.moveTo(40,40);window.resizeTo(947,592);window.location='https://t.co/kMzfpsXRGl';</script></body></html>" --no-sandbox --disable-session--crashed-bubble --incognito & sleep 3
+google-chrome --app="data:text/html,<html><body><script>window.moveTo(60,60);window.resizeTo(947,592);window.location='https://t.co/kMzfpsXRGl';</script></body></html>" --no-sandbox --disable-session--crashed-bubble --incognito & sleep 3
+google-chrome --app="data:text/html,<html><body><script>window.moveTo(80,80);window.resizeTo(947,592);window.location='https://t.co/kMzfpsXRGl';</script></body></html>" --no-sandbox --disable-session--crashed-bubble --incognito & sleep 3
+google-chrome --app="data:text/html,<html><body><script>window.moveTo(300,1);window.resizeTo(947,592);window.location='https://t.co/kMzfpsXRGl';</script></body></html>" --no-sandbox --disable-session--crashed-bubble --incognito & sleep 3
+google-chrome --app="data:text/html,<html><body><script>window.moveTo(320,20);window.resizeTo(947,592);window.location='https://t.co/kMzfpsXRGl';</script></body></html>" --no-sandbox --disable-session--crashed-bubble --incognito & sleep 3
+google-chrome --app="data:text/html,<html><body><script>window.moveTo(340,40);window.resizeTo(947,592);window.location='https://t.co/kMzfpsXRGl';</script></body></html>" --no-sandbox --disable-session--crashed-bubble --incognito & sleep 3
+google-chrome --app="data:text/html,<html><body><script>window.moveTo(360,60);window.resizeTo(947,592);window.location='https://t.co/kMzfpsXRGl';</script></body></html>" --no-sandbox --disable-session--crashed-bubble --incognito & sleep 3
+google-chrome --app="data:text/html,<html><body><script>window.moveTo(380,80);window.resizeTo(947,592);window.location='https://t.co/kMzfpsXRGl';</script></body></html>" --no-sandbox --disable-session--crashed-bubble --incognito & sleep 150
+killall chrome
+killall chrome
+killall chrome
+sleep 5
+done
