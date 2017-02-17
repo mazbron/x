635b22bd1d7ce62ba884891773273687d9bba177
Mazbron <mazbronads@gmail.com>
2017-02-18 00:02:30 +0700

    Update expert.sh

--- expert.sh
+++ expert.sh
@@ -10,7 +10,7 @@ google-chrome --user-data-dir=/home/chrome --app="data:text/html,<html><body><sc
 google-chrome --user-data-dir=/home/chrome --app="data:text/html,<html><body><script>window.moveTo(320,20);window.resizeTo(947,592);window.location='https://t.co/JZAHzFwSfM';</script></body></html>" --no-sandbox --disable-session--crashed-bubble --incognito & sleep 3
 google-chrome --user-data-dir=/home/chrome --app="data:text/html,<html><body><script>window.moveTo(340,40);window.resizeTo(947,592);window.location='https://t.co/JZAHzFwSfM';</script></body></html>" --no-sandbox --disable-session--crashed-bubble --incognito & sleep 3
 google-chrome --user-data-dir=/home/chrome --app="data:text/html,<html><body><script>window.moveTo(355,55);window.resizeTo(947,592);window.location='https://t.co/JZAHzFwSfM';</script></body></html>" --no-sandbox --disable-session--crashed-bubble --incognito & sleep 3
-google-chrome --user-data-dir=/home/chrome --app="data:text/html,<html><body><script>window.moveTo(365,365);window.resizeTo(947,592);window.location='https://t.co/JZAHzFwSfM';</script></body></html>" --no-sandbox --disable-session--crashed-bubble --incognito & sleep 100
+google-chrome --user-data-dir=/home/chrome --app="data:text/html,<html><body><script>window.moveTo(365,365);window.resizeTo(947,592);window.location='https://t.co/JZAHzFwSfM';</script></body></html>" --no-sandbox --disable-session--crashed-bubble --incognito & sleep 70
 killall chrome
 killall chrome
 killall chrome
