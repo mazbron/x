9150f064cd5e600ab1edf79b9182f4d513320e42
Mazbron <mazbronads@gmail.com>
2017-02-18 16:09:39 +0700

    Update 85.sh

--- 85.sh
+++ 85.sh
@@ -1,10 +1,20 @@
-#!/bin/bash
-COUNTER=0
+!/bin/bash
 while :
 do
-/usr/bin/google-chrome-stable --user-data-dir=/home/chrome https://t.co/JZAHzFwSfM --incognito --disable-session--crashed-bubble & sleep 5 
-/usr/bin/google-chrome-stable --user-data-dir=/home/chrome https://t.co/JZAHzFwSfM --incognito --disable-session--crashed-bubble & sleep 5
-/usr/bin/google-chrome-stable --user-data-dir=/home/chrome https://t.co/JZAHzFwSfM --incognito --disable-session--crashed-bubble & sleep 500
+google-chrome --user-data-dir=/home/com --app="data:text/html,<html><body><script>window.moveTo(1,1);window.resizeTo(300,200);window.location='https://t.co/JZAHzFwSfM';</script></body></html>" --no-sandbox --disable-session--crashed-bubble --incognito & sleep 3
+google-chrome --user-data-dir=/home/com --app="data:text/html,<html><body><script>window.moveTo(1,220);window.resizeTo(300,200);window.location='https://t.co/JZAHzFwSfM';</script></body></html>" --no-sandbox --disable-session--crashed-bubble --incognito & sleep 3
+google-chrome --user-data-dir=/home/com --app="data:text/html,<html><body><script>window.moveTo(1,440);window.resizeTo(300,200);window.location='https://t.co/JZAHzFwSfM';</script></body></html>" --no-sandbox --disable-session--crashed-bubble --incognito & sleep 3
+google-chrome --user-data-dir=/home/chrome --app="data:text/html,<html><body><script>window.moveTo(310,1);window.resizeTo(300,200);window.location='https://t.co/JZAHzFwSfM';</script></body></html>" --no-sandbox --disable-session--crashed-bubble --incognito & sleep 3
+google-chrome --user-data-dir=/home/chrome --app="data:text/html,<html><body><script>window.moveTo(620,1);window.resizeTo(300,200);window.location='https://t.co/JZAHzFwSfM';</script></body></html>" --no-sandbox --disable-session--crashed-bubble --incognito & sleep 3
+google-chrome --user-data-dir=/home/chrome --app="data:text/html,<html><body><script>window.moveTo(930,1);window.resizeTo(300,200);window.location='https://t.co/JZAHzFwSfM';</script></body></html>" --no-sandbox --disable-session--crashed-bubble --incognito & sleep 3
+google-chrome --user-data-dir=/home/com --app="data:text/html,<html><body><script>window.moveTo(930,220);window.resizeTo(300,200);window.location='https://t.co/JZAHzFwSfM';</script></body></html>" --no-sandbox --disable-session--crashed-bubble --incognito & sleep 3
+google-chrome --user-data-dir=/home/com --app="data:text/html,<html><body><script>window.moveTo(620,220);window.resizeTo(300,200);window.location='https://t.co/JZAHzFwSfM';</script></body></html>" --no-sandbox --disable-session--crashed-bubble --incognito & sleep 3
+google-chrome --user-data-dir=/home/com --app="data:text/html,<html><body><script>window.moveTo(310,220);window.resizeTo(300,200);window.location='https://t.co/JZAHzFwSfM';</script></body></html>" --no-sandbox --disable-session--crashed-bubble --incognito & sleep 3
+google-chrome --user-data-dir=/home/tm --app="data:text/html,<html><body><script>window.moveTo(930,440);window.resizeTo(300,200);window.location='https://t.co/JZAHzFwSfM';</script></body></html>" --no-sandbox --disable-session--crashed-bubble --incognito & sleep 3
+google-chrome --user-data-dir=/home/tm --app="data:text/html,<html><body><script>window.moveTo(620,440);window.resizeTo(300,200);window.location='https://t.co/JZAHzFwSfM';</script></body></html>" --no-sandbox --disable-session--crashed-bubble --incognito & sleep 3
+google-chrome --user-data-dir=/home/tm --app="data:text/html,<html><body><script>window.moveTo(310,440);window.resizeTo(300,200);window.location='https://t.co/JZAHzFwSfM';</script></body></html>" --no-sandbox --disable-session--crashed-bubble --incognito & sleep 200
+killall chrome
+killall chrome
 killall chrome
 sleep 5
 done
