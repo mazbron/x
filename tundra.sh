88c45d710bd6da467ac6513eb86536b97d9ed737
Mazbron <mazbronads@gmail.com>
2017-03-08 06:22:49 +0700

    Create tundra.sh

---
+++ tundra.sh
@@ -0,0 +1,20 @@
+!/bin/bash
+while :
+do
+google-chrome --user-data-dir=/home/tm --app="data:text/html,<html><body><script>window.moveTo(1,1);window.resizeTo(300,200);window.location='http://www.tundraheadquarters.com';</script></body></html>" --no-sandbox --disable-session--crashed-bubble --incognito & sleep 3
+google-chrome --user-data-dir=/home/tm --app="data:text/html,<html><body><script>window.moveTo(1,220);window.resizeTo(300,200);window.location='http://www.tundraheadquarters.com';</script></body></html>" --no-sandbox --disable-session--crashed-bubble --incognito & sleep 3
+google-chrome --user-data-dir=/home/tm --app="data:text/html,<html><body><script>window.moveTo(1,440);window.resizeTo(300,200);window.location='http://www.tundraheadquarters.com';</script></body></html>" --no-sandbox --disable-session--crashed-bubble --incognito & sleep 3
+google-chrome --user-data-dir=/home/tm --app="data:text/html,<html><body><script>window.moveTo(310,1);window.resizeTo(300,200);window.location='http://www.tundraheadquarters.com';</script></body></html>" --no-sandbox --disable-session--crashed-bubble --incognito & sleep 3
+google-chrome --user-data-dir=/home/tm --app="data:text/html,<html><body><script>window.moveTo(620,1);window.resizeTo(300,200);window.location='http://www.tundraheadquarters.com';</script></body></html>" --no-sandbox --disable-session--crashed-bubble --incognito & sleep 3
+google-chrome --user-data-dir=/home/tm --app="data:text/html,<html><body><script>window.moveTo(930,1);window.resizeTo(300,200);window.location='http://www.tundraheadquarters.com';</script></body></html>" --no-sandbox --disable-session--crashed-bubble --incognito & sleep 3
+google-chrome --user-data-dir=/home/tm --app="data:text/html,<html><body><script>window.moveTo(930,220);window.resizeTo(300,200);window.location='http://www.tundraheadquarters.com';</script></body></html>" --no-sandbox --disable-session--crashed-bubble --incognito & sleep 3
+google-chrome --user-data-dir=/home/tm --app="data:text/html,<html><body><script>window.moveTo(620,220);window.resizeTo(300,200);window.location='http://www.tundraheadquarters.com';</script></body></html>" --no-sandbox --disable-session--crashed-bubble --incognito & sleep 3
+google-chrome --user-data-dir=/home/tm --app="data:text/html,<html><body><script>window.moveTo(310,220);window.resizeTo(300,200);window.location='http://www.tundraheadquarters.com';</script></body></html>" --no-sandbox --disable-session--crashed-bubble --incognito & sleep 3
+google-chrome --user-data-dir=/home/tm --app="data:text/html,<html><body><script>window.moveTo(930,440);window.resizeTo(300,200);window.location='http://www.tundraheadquarters.com';</script></body></html>" --no-sandbox --disable-session--crashed-bubble --incognito & sleep 3
+google-chrome --user-data-dir=/home/tm --app="data:text/html,<html><body><script>window.moveTo(620,440);window.resizeTo(300,200);window.location='http://www.tundraheadquarters.com';</script></body></html>" --no-sandbox --disable-session--crashed-bubble --incognito & sleep 3
+google-chrome --user-data-dir=/home/tm --app="data:text/html,<html><body><script>window.moveTo(310,440);window.resizeTo(300,200);window.location='http://www.tundraheadquarters.com';</script></body></html>" --no-sandbox --disable-session--crashed-bubble --incognito & sleep 200
+killall chrome
+killall chrome
+killall chrome
+sleep 5
+done
