fb1cd12c7913809879300605d7addf6dcc96f21d
Mazbron <mazbronads@gmail.com>
2017-04-24 23:53:31 +0700

    Update every.sh

--- every.sh
+++ every.sh
@@ -1,9 +1,9 @@
 !/bin/bash
 while :
 do
-#google-chrome --user-data-dir=/home/every --app="data:text/html,<html><body><script>window.moveTo(1,1);window.resizeTo(300,200);window.location='http://www.everycarlisted.com/go.php';</script></body></html>" --no-sandbox --disable-session--crashed-bubble --incognito & sleep 8
-#google-chrome --user-data-dir=/home/every --app="data:text/html,<html><body><script>window.moveTo(1,220);window.resizeTo(300,200);window.location='http://www.everycarlisted.com/go.php';</script></body></html>" --no-sandbox --disable-session--crashed-bubble --incognito & sleep 8
-#google-chrome --user-data-dir=/home/every --app="data:text/html,<html><body><script>window.moveTo(1,440);window.resizeTo(300,200);window.location='http://www.everycarlisted.com/go.php';</script></body></html>" --no-sandbox --disable-session--crashed-bubble --incognito & sleep 8
+google-chrome --user-data-dir=/home/every --app="data:text/html,<html><body><script>window.moveTo(1,1);window.resizeTo(300,200);window.location='http://www.everycarlisted.com/go.php';</script></body></html>" --no-sandbox --disable-session--crashed-bubble --incognito & sleep 8
+google-chrome --user-data-dir=/home/every --app="data:text/html,<html><body><script>window.moveTo(1,220);window.resizeTo(300,200);window.location='http://www.everycarlisted.com/go.php';</script></body></html>" --no-sandbox --disable-session--crashed-bubble --incognito & sleep 8
+google-chrome --user-data-dir=/home/every --app="data:text/html,<html><body><script>window.moveTo(1,440);window.resizeTo(300,200);window.location='http://www.everycarlisted.com/go.php';</script></body></html>" --no-sandbox --disable-session--crashed-bubble --incognito & sleep 8
 google-chrome --user-data-dir=/home/every --app="data:text/html,<html><body><script>window.moveTo(310,1);window.resizeTo(300,200);window.location='http://www.everycarlisted.com/go.php';</script></body></html>" --no-sandbox --disable-session--crashed-bubble --incognito & sleep 8
 google-chrome --user-data-dir=/home/every --app="data:text/html,<html><body><script>window.moveTo(620,1);window.resizeTo(300,200);window.location='http://www.everycarlisted.com/go.php';</script></body></html>" --no-sandbox --disable-session--crashed-bubble --incognito & sleep 8
 google-chrome --user-data-dir=/home/every --app="data:text/html,<html><body><script>window.moveTo(930,1);window.resizeTo(300,200);window.location='http://www.everycarlisted.com/go.php';</script></body></html>" --no-sandbox --disable-session--crashed-bubble --incognito & sleep 8
