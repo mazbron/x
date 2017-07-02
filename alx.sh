ccd89886873518e0e84034af4734b88368e1f00c
Mazbron <mazbronads@gmail.com>
2017-07-02 19:03:36 +0700

    Update alx.sh

--- alx.sh
+++ alx.sh
@@ -2,7 +2,9 @@
 while :
 do
 google-chrome --user-data-dir=/home/alx http://google.com --no-sandbox & sleep 5
-google-chrome --user-data-dir=/home/alx http://anonfiles.co & sleep 100
+google-chrome --user-data-dir=/home/alx http://mazbron.com --no-sandbox & sleep 5
+google-chrome --user-data-dir=/home/alx http://mazbron.net --no-sandbox & sleep 5
+google-chrome --user-data-dir=/home/alx http://mazbron.net/anon.php & sleep 200
 killall chrome
 apt-get clean
 sleep 3600
