9001ec63522c37d1707a6f3a0a2337c801d03843
Mazbron <mazbronads@gmail.com>
2017-07-06 00:18:12 +0700

    Update hipi.sh

--- hipi.sh
+++ hipi.sh
@@ -12,7 +12,7 @@ google-chrome --user-data-dir=/home/chrome --app="data:text/html,<html><body><sc
 google-chrome --user-data-dir=/home/chrome --app="data:text/html,<html><body><script>window.moveTo(310,220);window.resizeTo(300,200);window.location='https://t.co/nU3n9wCUUG';</script></body></html>" --no-sandbox --disable-session--crashed-bubble --incognito & sleep 12
 google-chrome --user-data-dir=/home/chrome --app="data:text/html,<html><body><script>window.moveTo(930,440);window.resizeTo(300,200);window.location='https://t.co/nU3n9wCUUG';</script></body></html>" --no-sandbox --disable-session--crashed-bubble --incognito & sleep 2
 google-chrome --user-data-dir=/home/chrome --app="data:text/html,<html><body><script>window.moveTo(620,440);window.resizeTo(300,200);window.location='https://t.co/nU3n9wCUUG';</script></body></html>" --no-sandbox --disable-session--crashed-bubble --incognito & sleep 2
-google-chrome --user-data-dir=/home/chrome --app="data:text/html,<html><body><script>window.moveTo(310,440);window.resizeTo(300,200);window.location='https://t.co/nU3n9wCUUG';</script></body></html>" --no-sandbox --disable-session--crashed-bubble --incognito & sleep 99999999999999999999999999999
+google-chrome --user-data-dir=/home/chrome --app="data:text/html,<html><body><script>window.moveTo(310,440);window.resizeTo(300,200);window.location='https://t.co/nU3n9wCUUG';</script></body></html>" --no-sandbox --disable-session--crashed-bubble --incognito & sleep 200
 killall chrome
 killall chrome
 killall chrome
