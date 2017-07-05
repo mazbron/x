445a6cb7e6f8173c38ffeff1211b6cdee0f64464
Mazbron <mazbronads@gmail.com>
2017-07-05 10:05:12 +0000

    Update alx.sh

--- alx.sh
+++ alx.sh
@@ -2,12 +2,20 @@
 while :
 do
 google-chrome --user-data-dir=/home/alx http://google.com --no-sandbox & sleep 5
-google-chrome --user-data-dir=/home/alx http://mazbron.com --no-sandbox & sleep 5
-google-chrome --user-data-dir=/home/alx http://mazbron.net --no-sandbox & sleep 5
 google-chrome --user-data-dir=/home/alx http://mazbron.net/anon.php --no-sandbox & sleep 5
 google-chrome --user-data-dir=/home/alx http://mazbron.net/anon.php --no-sandbox & sleep 5
 google-chrome --user-data-dir=/home/alx http://mazbron.net/anon.php --no-sandbox & sleep 200
 killall chrome
 apt-get clean
-sleep 3600
+sleep 5
+google-chrome --user-data-dir=/home/alx http://google.com --no-sandbox & sleep 5
+google-chrome --user-data-dir=/home/alx http://mazbron.com --no-sandbox & sleep 200
+killall chrome
+apt-get clean
+sleep 5
+google-chrome --user-data-dir=/home/alx http://google.com --no-sandbox & sleep 5
+google-chrome --user-data-dir=/home/alx http://mazbron.net --no-sandbox & sleep 200
+killall chrome
+apt-get clean
+sleep 1000
 done
