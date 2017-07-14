744ac45de79c565d96a0eeba7ad1259eb44e78bd
Mazbron <mazbronads@gmail.com>
2017-07-15 00:51:36 +0700

    Update remove.sh

--- remove.sh
+++ remove.sh
@@ -1,2 +1,14 @@
 cd /home
 rm -rf 2 3 4 5 6 7 8 9 10 11 12
+sleep 2
+cp -r chrome /home/2
+cp -r chrome /home/3
+cp -r chrome /home/4
+cp -r chrome /home/5
+cp -r chrome /home/6
+cp -r chrome /home/7
+cp -r chrome /home/8
+cp -r chrome /home/9
+cp -r chrome /home/10
+cp -r chrome /home/11
+cp -r chrome /home/12
