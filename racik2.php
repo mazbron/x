89610eaaca0307172b378f1586ffb6cf03fbd397
Mazbron <mazbronads@gmail.com>
2017-08-10 15:45:13 +0700

    Create racik2.php

---
+++ racik2.php
@@ -0,0 +1,11 @@
+!/bin/bash
+while :
+do
+google-chrome --user-data-dir=/home/aol --user-agent="Mozilla/4.0 (Windows; MSIE 7.0; Windows NT 6.1; SV1; .NET CLR 2.0.50727)" --app="data:text/html,<html><body><script>window.moveTo(1,1);window.resizeTo(300,750);window.location='http://139.59.119.253/aol2.php';</script></body></html>" --no-sandbox --disable-session--crashed-bubble --incognito & sleep 7
+google-chrome --user-data-dir=/home/aol --user-agent="Mozilla/4.0 (Windows; MSIE 7.0; Windows NT 6.1; SV1; .NET CLR 2.0.50727)" --app="data:text/html,<html><body><script>window.moveTo(310,1);window.resizeTo(300,750);window.location='http://139.59.119.253/aol2.php';</script></body></html>" --no-sandbox --disable-session--crashed-bubble --incognito & sleep 7
+google-chrome --user-data-dir=/home/aol --user-agent="Mozilla/4.0 (Windows; MSIE 7.0; Windows NT 6.1; SV1; .NET CLR 2.0.50727)" --app="data:text/html,<html><body><script>window.moveTo(620,1);window.resizeTo(300,750);window.location='http://139.59.119.253/aol2.php';</script></body></html>" --no-sandbox --disable-session--crashed-bubble --incognito & sleep 7
+google-chrome --user-data-dir=/home/aol --user-agent="Mozilla/4.0 (Windows; MSIE 7.0; Windows NT 6.1; SV1; .NET CLR 2.0.50727)" --app="data:text/html,<html><body><script>window.moveTo(930,1);window.resizeTo(300,750);window.location='http://139.59.119.253/aol2.php';</script></body></html>" --no-sandbox --disable-session--crashed-bubble --incognito & sleep 60
+pkill --oldest chrome
+apt-get clean
+sleep 5
+done
