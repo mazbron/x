48c1ccfb37cc309e4d069e3edc18c5ec4e097af6
Mazbron <mazbronads@gmail.com>
2017-08-22 11:27:10 -0700

    Update AOL.sh

--- AOL.sh
+++ AOL.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 while :
 do
-#google-chrome --user-data-dir=/home/chrome --app="data:text/html,<html><body><script>window.moveTo(1,1);window.resizeTo(300,200);window.location='https://mazbron.com/aol.php';</script></body></html>" --no-sandbox --disable-session--crashed-bubble --incognito & sleep 3
-#google-chrome --user-data-dir=/home/chrome --app="data:text/html,<html><body><script>window.moveTo(1,220);window.resizeTo(300,200);window.location='https://mazbron.com/aol.php';</script></body></html>" --no-sandbox --disable-session--crashed-bubble --incognito & sleep 3
-#google-chrome --user-data-dir=/home/chrome --app="data:text/html,<html><body><script>window.moveTo(1,440);window.resizeTo(300,200);window.location='https://mazbron.com/aol.php';</script></body></html>" --no-sandbox --disable-session--crashed-bubble --incognito & sleep 10
+google-chrome --user-data-dir=/home/chrome --app="data:text/html,<html><body><script>window.moveTo(1,1);window.resizeTo(300,200);window.location='https://mazbron.com/aol.php';</script></body></html>" --no-sandbox --disable-session--crashed-bubble --incognito & sleep 3
+google-chrome --user-data-dir=/home/chrome --app="data:text/html,<html><body><script>window.moveTo(1,220);window.resizeTo(300,200);window.location='https://mazbron.com/aol.php';</script></body></html>" --no-sandbox --disable-session--crashed-bubble --incognito & sleep 3
+google-chrome --user-data-dir=/home/chrome --app="data:text/html,<html><body><script>window.moveTo(1,440);window.resizeTo(300,200);window.location='https://mazbron.com/aol.php';</script></body></html>" --no-sandbox --disable-session--crashed-bubble --incognito & sleep 10
 google-chrome --user-data-dir=/home/chrome --app="data:text/html,<html><body><script>window.moveTo(310,1);window.resizeTo(300,200);window.location='https://mazbron.com/aol.php';</script></body></html>" --no-sandbox --disable-session--crashed-bubble --incognito & sleep 3
 google-chrome --user-data-dir=/home/chrome --app="data:text/html,<html><body><script>window.moveTo(620,1);window.resizeTo(300,200);window.location='https://mazbron.com/aol.php';</script></body></html>" --no-sandbox --disable-session--crashed-bubble --incognito & sleep 3
 google-chrome --user-data-dir=/home/chrome --app="data:text/html,<html><body><script>window.moveTo(930,1);window.resizeTo(300,200);window.location='https://mazbron.com/aol.php';</script></body></html>" --no-sandbox --disable-session--crashed-bubble --incognito & sleep 10
@@ -12,8 +12,8 @@ google-chrome --user-data-dir=/home/chrome --app="data:text/html,<html><body><sc
 google-chrome --user-data-dir=/home/chrome --app="data:text/html,<html><body><script>window.moveTo(310,220);window.resizeTo(300,200);window.location='https://mazbron.com/aol.php';</script></body></html>" --no-sandbox --disable-session--crashed-bubble --incognito & sleep 10
 google-chrome --user-data-dir=/home/chrome --app="data:text/html,<html><body><script>window.moveTo(930,440);window.resizeTo(300,200);window.location='https://mazbron.com/aol.php';</script></body></html>" --no-sandbox --disable-session--crashed-bubble --incognito & sleep 3
 google-chrome --user-data-dir=/home/chrome --app="data:text/html,<html><body><script>window.moveTo(620,440);window.resizeTo(300,200);window.location='https://mazbron.com/aol.php';</script></body></html>" --no-sandbox --disable-session--crashed-bubble --incognito & sleep 3
-google-chrome --user-data-dir=/home/chrome --app="data:text/html,<html><body><script>window.moveTo(310,440);window.resizeTo(300,200);window.location='https://mazbron.com/aol.php';</script></body></html>" --no-sandbox --disable-session--crashed-bubble --incognito & sleep 210
+google-chrome --user-data-dir=/home/chrome --app="data:text/html,<html><body><script>window.moveTo(310,440);window.resizeTo(300,200);window.location='https://mazbron.com/aol.php';</script></body></html>" --no-sandbox --disable-session--crashed-bubble --incognito & sleep 120
 pkill --oldest chrome
 apt-get clean
-sleep 10
+sleep 7
 done
