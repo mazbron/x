dc8c3c50f5cb6ea91d3d9648792abef0b02b8bc8
Mazbron <mazbronads@gmail.com>
2019-05-28 23:14:44 +0000

    Add files via upload

---
+++ smarty.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+while :
+do
+#!/bin/bash
+while true
+do
+proxychains firefox http://lazypenguins.com & sleep 7
+proxychains firefox http://lazypenguins.com -P 2 & sleep 7
+proxychains firefox http://lazypenguins.com -P 3 & sleep 80
+
+killall firefox
+sleep 5
+done
+apt-get clean
+sleep 7
+done
