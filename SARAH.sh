461bdc5dabe3e47efac430bd1964a7205e015c52
Mazbron <mazbronads@gmail.com>
2019-06-11 09:29:38 +0000

    Add files via upload

---
+++ SARAH.sh
@@ -0,0 +1,16 @@
+!/bin/bash
+while :
+do
+proxychains google-chrome --user-data-dir=/home/chrome -app="data:text/html,<html><body><script>window.moveTo(1,1);window.resizeTo(800,600);window.location='http://halloween.wtf/sarah.html';</script></body></html>" --no-sandbox --disable-session--crashed-bubble --incognito & sleep 5
+proxychains google-chrome --user-data-dir=/home/chrome2 -app="data:text/html,<html><body><script>window.moveTo(800,1);window.resizeTo(800,600);window.location='http://halloween.wtf/sarah.html';</script></body></html>" --no-sandbox --disable-session--crashed-bubble --incognito & sleep 50
+apt-get clean
+killall chrome
+killall chrome
+sleep 5
+proxychains google-chrome --user-data-dir=/home/chrome3 -app="data:text/html,<html><body><script>window.moveTo(1,1);window.resizeTo(800,600);window.location='http://halloween.wtf/sarah.html';</script></body></html>" --no-sandbox --disable-session--crashed-bubble --incognito & sleep 5
+proxychains google-chrome --user-data-dir=/home/chrome4 -app="data:text/html,<html><body><script>window.moveTo(800,1);window.resizeTo(800,600);window.location='http://halloween.wtf/sarah.html';</script></body></html>" --no-sandbox --disable-session--crashed-bubble --incognito & sleep 60
+apt-get clean
+killall chrome
+killall chrome
+sleep 5
+done
