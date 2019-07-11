a1931e5fb84891e35c69d547eec36f9fb5f0eea8
Mazbron <9880408+mazbron@users.noreply.github.com>
2019-07-11 17:03:37 +0700

    Update SARAH2.sh

--- SARAH2.sh
+++ SARAH2.sh
@@ -14,7 +14,7 @@ apt-get autoclean
 google-chrome --user-data-dir=/home/chrome --profile-directory="Profile 2" --force-android-app-mode --app="data:text/html,<html><body><script>window.moveTo(620,1);window.resizeTo(300,200);window.location='https://t.co/RBqV3BOrs9';</script></body></html>" --no-sandbox --disable-session--crashed-bubble --enforce-webrtc-ip-permission-check --allow-insecure-localhost --proxy-auto-detect  --force-app-mode --incognito & sleep 3
 clear
 apt-get autoclean
-google-chrome --user-data-dir=/home/chrome --profile-directory="Profile 3" --force-android-app-mode --app="data:text/html,<html><body><script>window.moveTo(930,1);window.resizeTo(300,200);window.location='https://t.co/RBqV3BOrs9';</script></body></html>" --no-sandbox --disable-session--crashed-bubble --enforce-webrtc-ip-permission-check --allow-insecure-localhost --proxy-auto-detect ---force-app-mode -incognito & sleep 3
+google-chrome --user-data-dir=/home/chrome --profile-directory="Profile 3" --force-android-app-mode --app="data:text/html,<html><body><script>window.moveTo(935,1);window.resizeTo(300,200);window.location='https://t.co/RBqV3BOrs9';</script></body></html>" --no-sandbox --disable-session--crashed-bubble --enforce-webrtc-ip-permission-check --allow-insecure-localhost --proxy-auto-detect ---force-app-mode -incognito & sleep 3
 clear
 echo SLEEP 35s
 sleep 35
