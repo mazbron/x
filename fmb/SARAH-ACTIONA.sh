047add618ff92117ed464afc09e94b7e1c970bab
Mazbron <9880408+mazbron@users.noreply.github.com>
2019-07-17 14:27:39 +0700

    Update SARAH-ACTIONA.sh

--- fmb/SARAH-ACTIONA.sh
+++ fmb/SARAH-ACTIONA.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 while :
 do
-google-chrome --user-data-dir=/home/chrome --window-size=470,728 --window-position=1214,1 --app=HTTP://SARAHVILO.ID/puzzle/wake-the-santa --no-sandbox --disable-session--crashed-bubble --incognito & sleep 1
+google-chrome --user-data-dir=/home/chrome --window-size=470,728 --window-position=1214,1 --force-android-app-mode --app=HTTP://SARAHVILO.ID/puzzle/wake-the-santa --no-sandbox --disable-session--crashed-bubble --incognito & sleep 1
 clear
 sleep 5
 clear
