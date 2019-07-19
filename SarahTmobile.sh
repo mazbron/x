209c7e2014bed2fdf79256c95c0f9d39a3468c0f
Mazbron <9880408+mazbron@users.noreply.github.com>
2019-07-20 00:12:15 +0700

    Create SarahTmobile.sh

---
+++ SarahTmobile.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+while :
+do
+pkill --oldest chrome
+pkill --oldest chrome
+pkill --oldest chrome
+pkill --oldest chrome
+proxychains google-chrome --user-data-dir=/home/chrome2 --profile-directory="Default" --app=https://t.co/bEVs0g8sED --window-size=450,650 --window-position=1229,1 --no-sandbox --disable-session--crashed-bubble  - -incognito & sleep 3
+clear
+google-chrome --user-data-dir=/home/chrome --profile-directory="Default"  --app="data:text/html,<html><body><script>window.moveTo(1,1);window.resizeTo(300,200);window.location='https://t.co/RBqV3BOrs9';</script></body></html>" --no-sandbox --disable-session--crashed-bubble  --incognito & sleep 3
+clear
+apt-get autoclean
+google-chrome --user-data-dir=/home/chrome --profile-directory="Default"   --app="data:text/html,<html><body><script>window.moveTo(310,1);window.resizeTo(300,200);window.location='https://t.co/RBqV3BOrs9';</script></body></html>" --no-sandbox --disable-session--crashed-bubble   --force-app-mode --incognito & sleep 3 
+clear
+apt-get autoclean
+google-chrome --user-data-dir=/home/chrome --profile-directory="Default"   --app="data:text/html,<html><body><script>window.moveTo(620,1);window.resizeTo(300,200);window.location='https://t.co/RBqV3BOrs9';</script></body></html>" --no-sandbox --disable-session--crashed-bubble   --force-app-mode --incognito & sleep 3
+clear
+echo SKIP AD
+xdotool mousemove 1622 625 click 1
+apt-get autoclean
+google-chrome --user-data-dir=/home/chrome --profile-directory="Default"   --app="data:text/html,<html><body><script>window.moveTo(935,1);window.resizeTo(300,200);window.location='https://t.co/RBqV3BOrs9';</script></body></html>" --no-sandbox --disable-session--crashed-bubble  ---force-app-mode -incognito & sleep 3
+clear
+echo SLEEP 35s
+sleep 2
+echo click GO
+xdotool mousemove 1466 434 click 1
+sleep 3
+echo play
+xdotool mousemove 1464 555 click 1
+xdotool mousemove 1464 555 click 1
+sleep 0.5
+xdotool mousemove 1464 555 click 1
+sleep 0.5
+xdotool mousemove 1464 555 click 1
+sleep 0.5
+xdotool mousemove 1464 555 click 1
+sleep 0.5
+xdotool mousemove 1464 555 click 1
+sleep 0.5
+xdotool mousemove 1464 555 click 1
+sleep 0.5
+xdotool mousemove 1464 555 click 1
+sleep 0.5
+xdotool mousemove 1464 555 click 1
+sleep 0.5
+xdotool mousemove 1464 555 click 1
+sleep 0.5
+xdotool mousemove 1464 555 click 1
+sleep 0.5
+xdotool mousemove 1464 555 click 1
+xdotool mousemove 1464 555 click 1
+xdotool mousemove 1464 555 click 1
+xdotool mousemove 1464 555 click 1
+xdotool mousemove 1464 555 click 1
+xdotool mousemove 1464 555 click 1
+xdotool mousemove 1464 555 click 1
+xdotool mousemove 1464 555 click 1
+sleep 0.5
+echo wait 25s
+sleep 22
+pkill --oldest chrome
+pkill --oldest chrome
+pkill --oldest chrome
+pkill --oldest chrome
+pkill --oldest chrome
+apt-get autoclean
+clear
+sleep 5
+done
