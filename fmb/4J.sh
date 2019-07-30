a2bd4d6deb35371ae3e97da5de07e0456550064c
Mazbron <9880408+mazbron@users.noreply.github.com>
2019-07-31 00:07:33 +0700

    Add files via upload

---
+++ fmb/4J.sh
@@ -0,0 +1,217 @@
+#!/bin/bash
+printf '\033]2;:P\a'
+while :
+do
+pkill --oldest chrome
+echo 1 
+proxychains google-chrome --user-data-dir=/home/chrome2 --force-android-app-mode --app=http://halloween.wtf/4J.php --window-size=390,650 --window-position=1,1 --no-sandbox --incognito & sleep 1
+clear 
+sleep 1
+clear 
+echo Wait 19
+sleep 1
+clear 
+echo Wait 18
+sleep 1
+clear 
+echo Wait 17
+sleep 1
+clear 
+echo Wait 16
+sleep 1
+clear 
+echo Wait 15
+sleep 1
+clear 
+echo Wait 14
+sleep 1
+clear 
+echo Wait 13
+sleep 1
+clear 
+echo Wait 12
+sleep 1
+clear 
+echo Wait 11
+sleep 1
+clear 
+echo Wait 10
+sleep 1
+clear 
+echo Wait 9
+sleep 1
+clear 
+echo Wait 8
+sleep 1
+clear 
+echo Wait 7
+sleep 1
+clear 
+echo Wait 6
+sleep 1
+clear 
+echo Wait 5
+sleep 1
+clear 
+echo Wait 4
+sleep 1
+clear 
+echo Wait 3
+sleep 1
+clear 
+echo Wait 2
+sleep 1
+clear 
+echo Wait 1
+sleep 1
+clear
+#set -x 
+echo skip ad
+xdotool mousemove 368 624 click 1 
+clear
+echo skip ad done
+sleep 2
+clear
+echo Play button
+xdotool mousemove 222 436 click 1 
+sleep 2
+xdotool mousemove 199 522 click 1 
+sleep 2
+clear
+echo Play
+xdotool mousemove 199 522 click 1 
+sleep 0.5
+xdotool mousemove 199 522 click 1 
+sleep 0.5
+xdotool mousemove 199 522 click 1 
+sleep 0.5
+xdotool mousemove 199 522 click 1 
+sleep 0.5
+xdotool mousemove 199 522 click 1 
+sleep 0.5
+xdotool mousemove 199 522 click 1 
+sleep 0.5
+xdotool mousemove 199 522 click 1 
+sleep 0.5
+xdotool mousemove 199 522 click 1 
+xdotool mousemove 199 522 click 1 
+sleep 0.5
+xdotool mousemove 199 522 click 1 
+sleep 0.5
+xdotool mousemove 199 522 click 1 
+sleep 0.5
+xdotool mousemove 199 522 click 1 
+sleep 0.5
+xdotool mousemove 199 522 click 1 
+sleep 0.5
+xdotool mousemove 199 522 click 1 
+sleep 0.5
+xdotool mousemove 199 522 click 1 
+xdotool mousemove 199 522 click 1 
+xdotool mousemove 199 522 click 1 
+xdotool mousemove 199 522 click 1 
+xdotool mousemove 199 522 click 1 
+xdotool mousemove 199 522 click 1 
+xdotool mousemove 199 522 click 1 
+xdotool mousemove 199 522 click 1 
+xdotool mousemove 199 522 click 1 
+xdotool mousemove 199 522 click 1 
+sleep 0.5
+sleep 3
+clear
+echo watch ad
+xdotool mousemove 182 528 click 1 
+sleep 8
+clear
+echo skip ad
+clear
+echo skip ad done
+xdotool mousemove 368 624 click 1 
+sleep 3
+clear
+echo Play
+xdotool mousemove 199 522 click 1 
+sleep 0.5
+xdotool mousemove 199 522 click 1 
+sleep 0.5
+xdotool mousemove 199 522 click 1 
+sleep 0.5
+xdotool mousemove 199 522 click 1 
+sleep 0.5
+xdotool mousemove 199 522 click 1 
+sleep 0.5
+xdotool mousemove 199 522 click 1 
+sleep 0.5
+xdotool mousemove 199 522 click 1 
+sleep 0.5
+xdotool mousemove 199 522 click 1 
+sleep 0.5
+xdotool mousemove 199 522 click 1 
+xdotool mousemove 199 522 click 1 
+xdotool mousemove 199 522 click 1 
+xdotool mousemove 199 522 click 1 
+xdotool mousemove 199 522 click 1 
+xdotool mousemove 199 522 click 1 
+sleep 0.5
+clear
+echo click X
+xdotool mousemove 367 82 click 1
+sleep 3
+clear
+echo restart
+xdotool mousemove 208 529 click 1 
+sleep 3
+clear
+echo Play
+xdotool mousemove 199 522 click 1 
+sleep 0.5
+xdotool mousemove 199 522 click 1 
+sleep 0.5
+xdotool mousemove 199 522 click 1 
+sleep 0.5
+xdotool mousemove 199 522 click 1 
+sleep 0.5
+xdotool mousemove 199 522 click 1 
+sleep 0.5
+xdotool mousemove 199 522 click 1 
+sleep 0.5
+xdotool mousemove 199 522 click 1 
+sleep 0.5
+xdotool mousemove 199 522 click 1 
+sleep 0.5
+xdotool mousemove 199 522 click 1 
+sleep 0.5
+xdotool mousemove 199 522 click 1 
+sleep 0.5
+xdotool mousemove 199 522 click 1 
+sleep 0.5
+xdotool mousemove 199 522 click 1 
+sleep 0.5
+xdotool mousemove 199 522 click 1 
+sleep 0.5
+xdotool mousemove 199 522 click 1 
+sleep 0.5
+xdotool mousemove 199 522 click 1 
+sleep 0.5
+xdotool mousemove 199 522 click 1 
+sleep 0.5
+xdotool mousemove 199 522 click 1 
+sleep 0.5
+xdotool mousemove 199 522 click 1 
+sleep 0.5
+xdotool mousemove 199 522 click 1 
+sleep 0.5
+clear
+sleep 3
+echo watch ad
+xdotool mousemove 182 528 click 1 
+sleep 5
+pkill --oldest chrome
+pkill --oldest chrome
+pkill --oldest chrome
+apt-get clean
+sleep 2
+clear
+done
+
+
