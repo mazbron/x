896eaaebed24ae402429307b664a5b7bb2fedaf6
Mazbron <9880408+mazbron@users.noreply.github.com>
2019-07-31 20:46:26 +0700

    Add files via upload

---
+++ 4JActiona.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+printf '\033]2;helpbot\a'
+while :
+do
+proxychains chromium-browser --user-data-dir=/home/chrome2 --force-android-app-mode --app=http://halloween.wtf/4J.php --window-size=390,650 --window-position=1299,1 --no-sandbox --incognito & sleep 1
+clear
+sleep 17
+clear
+echo start helpbot
+actiona -s -C -Q -e -x /root/Desktop/lib/4j.ascr
+sleep 7
+killall chromium-browser
+apt-get autoclean
+clear
+sleep 3
+/usr/games/sl -l
+sleep 2
+done
