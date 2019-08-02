6c953923c99e4f20f14f882d8784322346bcabd7
Mazbron <9880408+mazbron@users.noreply.github.com>
2019-08-02 22:31:46 +0700

    Update SARAH-ACTIONA.sh

--- fmb/SARAH-ACTIONA.sh
+++ fmb/SARAH-ACTIONA.sh
@@ -3,7 +3,7 @@ while :
 do
 proxychains google-chrome --user-data-dir=/home/chrome --window-size=470,728 --window-position=1214,1 --force-android-app-mode --app=HTTP://SARAHVILO.ID/puzzle/wake-the-santa --no-sandbox --disable-session--crashed-bubble --incognito & sleep 1
 clear
-sleep 5
+sleep 10
 clear
 echo START ACTIONA
 actiona -s -C -Q -e -x PLAY.ascr
