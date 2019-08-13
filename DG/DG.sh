e07ec7b543b4a79365e1f6fe2e863819bb505239
Mazbron <9880408+mazbron@users.noreply.github.com>
2019-08-13 14:15:02 -0500

    Add files via upload

---
+++ DG/DG.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+while :
+do
+pkill --oldest chrome
+pkill --oldest chrome
+pkill --oldest chrome
+pkill --oldest chrome
+pkill --oldest chrome
+pkill --oldest chrome
+proxychains google-chrome --user-data-dir=/home/chrome60 --window-size=720,455 --window-position=1,1 --force-android-app-mode --app=HTTP://play.SARAHVILO.ID/stickwar.html --no-sandbox --disable-session--crashed-bubble --incognito & sleep 1
+proxychains google-chrome --user-data-dir=/home/chrome602 --window-size=720,455 --window-position=971,1 --force-android-app-mode --app=HTTP://play.SARAHVILO.ID/stickwar.html --no-sandbox --disable-session--crashed-bubble --incognito & sleep 1
+clear
+sleep 12
+clear
+echo START ACTIONA
+actiona -s -C -Q -e -x DG.ascr
+sleep 2
+pkill --oldest chrome
+pkill --oldest chrome
+pkill --oldest chrome
+pkill --oldest chrome
+pkill --oldest chrome
+pkill --oldest chrome
+apt-get autoclean
+clear
+sleep 5
+done
