2b4292d1f275c44694424ea38405f384d6eba52f
Mazbron <9880408+mazbron@users.noreply.github.com>
2019-08-25 10:16:08 +0700

    Update alx2.sh

--- alx2.sh
+++ alx2.sh
@@ -5,43 +5,44 @@ pkill --oldest chrome
 pkill --oldest chrome
 pkill --oldest chrome
 pkill --oldest chrome
-proxychains google-chrome --user-data-dir=/home/alx --profile-directory="Default" --app="data:text/html,<html><body><script>window.moveTo(1,1);window.resizeTo(800,550);window.location='http://www.google.com';</script></body></html>" --no-sandbox --disable-session--crashed-bubble --enforce-webrtc-ip-permission-check --allow-insecure-localhost --proxy-auto-detect --incognito & sleep 3
+proxychains google-chrome --user-data-dir=/home/chrome60 --profile-directory="Default" --app="data:text/html,<html><body><script>window.moveTo(1,1);window.resizeTo(800,550);window.location='http://www.google.com';</script></body></html>" --no-sandbox --disable-session--crashed-bubble --enforce-webrtc-ip-permission-check --allow-insecure-localhost --proxy-auto-detect --incognito & sleep 3
 clear
 apt-get autoclean
-proxychains google-chrome --user-data-dir=/home/alx --profile-directory="Profile 4" --app="data:text/html,<html><body><script>window.moveTo(1,553);window.resizeTo(800,550);window.location='http://www.google.com';</script></body></html>" --no-sandbox --disable-session--crashed-bubble --enforce-webrtc-ip-permission-check --allow-insecure-localhost --proxy-auto-detect --incognito & sleep 3 
+proxychains google-chrome --user-data-dir=/home/chrome602 --profile-directory="Default" --app="data:text/html,<html><body><script>window.moveTo(1,553);window.resizeTo(800,550);window.location='http://www.google.com';</script></body></html>" --no-sandbox --disable-session--crashed-bubble --enforce-webrtc-ip-permission-check --allow-insecure-localhost --proxy-auto-detect --incognito & sleep 3 
 clear
 apt-get autoclean
-proxychains google-chrome --user-data-dir=/home/alx --profile-directory="Profile 2" --app="data:text/html,<html><body><script>window.moveTo(813,1);window.resizeTo(800,550);window.location='http://www.google.com';</script></body></html>" --no-sandbox --disable-session--crashed-bubble --enforce-webrtc-ip-permission-check --allow-insecure-localhost --proxy-auto-detect --incognito & sleep 3
+proxychains google-chrome --user-data-dir=/home/chrome603 --profile-directory="Default" --app="data:text/html,<html><body><script>window.moveTo(813,1);window.resizeTo(800,550);window.location='http://www.google.com';</script></body></html>" --no-sandbox --disable-session--crashed-bubble --enforce-webrtc-ip-permission-check --allow-insecure-localhost --proxy-auto-detect --incognito & sleep 3
 clear
 apt-get autoclean
-proxychains google-chrome --user-data-dir=/home/alx --profile-directory="Profile 3" --app="data:text/html,<html><body><script>window.moveTo(808,553);window.resizeTo(800,550);window.location='http://www.google.com';</script></body></html>" --no-sandbox --disable-session--crashed-bubble --enforce-webrtc-ip-permission-check --allow-insecure-localhost --proxy-auto-detect -incognito & sleep 3
+proxychains google-chrome --user-data-dir=/home/chrome604 --profile-directory="Default" --app="data:text/html,<html><body><script>window.moveTo(808,553);window.resizeTo(800,550);window.location='http://www.google.com';</script></body></html>" --no-sandbox --disable-session--crashed-bubble --enforce-webrtc-ip-permission-check --allow-insecure-localhost --proxy-auto-detect -incognito & sleep 3
 clear
 echo Leren sek 30s
 sleep 30
-proxychains google-chrome --user-data-dir=/home/alx --profile-directory="Default" --app="data:text/html,<html><body><script>window.moveTo(1,1);window.resizeTo(800,550);window.location='http://www.google.com';</script></body></html>" --no-sandbox --disable-session--crashed-bubble --enforce-webrtc-ip-permission-check --allow-insecure-localhost --proxy-auto-detect --incognito & sleep 3
+proxychains google-chrome --user-data-dir=/home/chrome60 --profile-directory="Default" --app="data:text/html,<html><body><script>window.moveTo(1,1);window.resizeTo(800,550);window.location='http://www.google.com';</script></body></html>" --no-sandbox --disable-session--crashed-bubble --enforce-webrtc-ip-permission-check --allow-insecure-localhost --proxy-auto-detect --incognito & sleep 3
 clear
 apt-get autoclean
-proxychains google-chrome --user-data-dir=/home/alx --profile-directory="Profile 4" --app="data:text/html,<html><body><script>window.moveTo(1,553);window.resizeTo(800,550);window.location='http://www.google.com';</script></body></html>" --no-sandbox --disable-session--crashed-bubble --enforce-webrtc-ip-permission-check --allow-insecure-localhost --proxy-auto-detect --incognito & sleep 3 
+proxychains google-chrome --user-data-dir=/home/chrome602 --profile-directory="Default" --app="data:text/html,<html><body><script>window.moveTo(1,553);window.resizeTo(800,550);window.location='http://www.google.com';</script></body></html>" --no-sandbox --disable-session--crashed-bubble --enforce-webrtc-ip-permission-check --allow-insecure-localhost --proxy-auto-detect --incognito & sleep 3 
 clear
 apt-get autoclean
-proxychains google-chrome --user-data-dir=/home/alx --profile-directory="Profile 2" --app="data:text/html,<html><body><script>window.moveTo(813,1);window.resizeTo(800,550);window.location='http://www.google.com';</script></body></html>" --no-sandbox --disable-session--crashed-bubble --enforce-webrtc-ip-permission-check --allow-insecure-localhost --proxy-auto-detect --incognito & sleep 3
+proxychains google-chrome --user-data-dir=/home/chrome603 --profile-directory="Default" --app="data:text/html,<html><body><script>window.moveTo(813,1);window.resizeTo(800,550);window.location='http://www.google.com';</script></body></html>" --no-sandbox --disable-session--crashed-bubble --enforce-webrtc-ip-permission-check --allow-insecure-localhost --proxy-auto-detect --incognito & sleep 3
 clear
 apt-get autoclean
-proxychains google-chrome --user-data-dir=/home/alx --profile-directory="Profile 3" --app="data:text/html,<html><body><script>window.moveTo(808,553);window.resizeTo(800,550);window.location='http://www.google.com';</script></body></html>" --no-sandbox --disable-session--crashed-bubble --enforce-webrtc-ip-permission-check --allow-insecure-localhost --proxy-auto-detect -incognito & sleep 3
+proxychains google-chrome --user-data-dir=/home/chrome604 --profile-directory="Default" --app="data:text/html,<html><body><script>window.moveTo(808,553);window.resizeTo(800,550);window.location='http://www.google.com';</script></body></html>" --no-sandbox --disable-session--crashed-bubble --enforce-webrtc-ip-permission-check --allow-insecure-localhost --proxy-auto-detect -incognito & sleep 3
 clear
 echo Leren sek 30s
 sleep 30
-proxychains google-chrome --user-data-dir=/home/alx --profile-directory="Default" --app="data:text/html,<html><body><script>window.moveTo(1,1);window.resizeTo(800,550);window.location='http://www.google.com';</script></body></html>" --no-sandbox --disable-session--crashed-bubble --enforce-webrtc-ip-permission-check --allow-insecure-localhost --proxy-auto-detect --incognito & sleep 3
+proxychains google-chrome --user-data-dir=/home/chrome60 --profile-directory="Default" --app="data:text/html,<html><body><script>window.moveTo(1,1);window.resizeTo(800,550);window.location='http://www.google.com';</script></body></html>" --no-sandbox --disable-session--crashed-bubble --enforce-webrtc-ip-permission-check --allow-insecure-localhost --proxy-auto-detect --incognito & sleep 3
 clear
 apt-get autoclean
-proxychains google-chrome --user-data-dir=/home/alx --profile-directory="Profile 4" --app="data:text/html,<html><body><script>window.moveTo(1,553);window.resizeTo(800,550);window.location='http://www.google.com';</script></body></html>" --no-sandbox --disable-session--crashed-bubble --enforce-webrtc-ip-permission-check --allow-insecure-localhost --proxy-auto-detect --incognito & sleep 3 
+proxychains google-chrome --user-data-dir=/home/chrome602 --profile-directory="Default" --app="data:text/html,<html><body><script>window.moveTo(1,553);window.resizeTo(800,550);window.location='http://www.google.com';</script></body></html>" --no-sandbox --disable-session--crashed-bubble --enforce-webrtc-ip-permission-check --allow-insecure-localhost --proxy-auto-detect --incognito & sleep 3 
 clear
 apt-get autoclean
-proxychains google-chrome --user-data-dir=/home/alx --profile-directory="Profile 2" --app="data:text/html,<html><body><script>window.moveTo(813,1);window.resizeTo(800,550);window.location='http://www.google.com';</script></body></html>" --no-sandbox --disable-session--crashed-bubble --enforce-webrtc-ip-permission-check --allow-insecure-localhost --proxy-auto-detect --incognito & sleep 3
+proxychains google-chrome --user-data-dir=/home/chrome603 --profile-directory="Default" --app="data:text/html,<html><body><script>window.moveTo(813,1);window.resizeTo(800,550);window.location='http://www.google.com';</script></body></html>" --no-sandbox --disable-session--crashed-bubble --enforce-webrtc-ip-permission-check --allow-insecure-localhost --proxy-auto-detect --incognito & sleep 3
 clear
 apt-get autoclean
-proxychains google-chrome --user-data-dir=/home/alx --profile-directory="Profile 3" --app="data:text/html,<html><body><script>window.moveTo(808,553);window.resizeTo(800,550);window.location='http://www.google.com';</script></body></html>" --no-sandbox --disable-session--crashed-bubble --enforce-webrtc-ip-permission-check --allow-insecure-localhost --proxy-auto-detect -incognito & sleep 3
+proxychains google-chrome --user-data-dir=/home/chrome604 --profile-directory="Default" --app="data:text/html,<html><body><script>window.moveTo(808,553);window.resizeTo(800,550);window.location='http://www.google.com';</script></body></html>" --no-sandbox --disable-session--crashed-bubble --enforce-webrtc-ip-permission-check --allow-insecure-localhost --proxy-auto-detect -incognito & sleep 3
 clear
+echo Leren sek 60s
 echo SLEEP 60s
 sleep 60
 pkill --oldest chrome
