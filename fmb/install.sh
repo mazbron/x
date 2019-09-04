222bfab25f66e0ccbf471f89a3eda33d65196c0a
Mazbron <9880408+mazbron@users.noreply.github.com>
2019-09-04 16:29:23 +0700

    Update install.sh

--- fmb/install.sh
+++ fmb/install.sh
@@ -34,7 +34,7 @@ sleep 1
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
 sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
 sudo apt-get update -y
-#sudo apt-get install google-chrome-stable -y
+sudo apt-get install google-chrome-stable -y
 wget http://128.199.103.32/x/chrome60.deb
 sudo dpkg -i chrome60.deb
 
