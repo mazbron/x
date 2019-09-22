fd16c65f5dc95fe122dcc861365d021336f4f973
Mazbron <9880408+mazbron@users.noreply.github.com>
2019-09-22 17:11:45 +0700

    Update atozmoneytizer.sh

--- money/atozmoneytizer.sh
+++ money/atozmoneytizer.sh
@@ -4,7 +4,7 @@ while :
 do
 rm -rf /home/chrome60/Default/Cache/*
 pkill --oldest chrome
-killall google-chrome
+
  google-chrome --user-data-dir=/home/chrome60 --force-android-app-mode https://t.co/bMuwrPTAf8 --window-size=1300,725 --no-sandbox & sleep 1
 clear
 sleep 100
@@ -13,7 +13,7 @@ clear
 #actiona -s -C -Q -e -x 4JAtoZGame.ascr
 #sleep 7
 pkill --oldest chrome
-killall google-chrome
+
 apt-get autoclean
 clear
 sleep 5
