f60199a81aefb4cfabfb51a78bd6975d6321d2ff
Mazbron <9880408+mazbron@users.noreply.github.com>
2019-09-25 17:05:28 +0700

    Update get.sh

--- money/get.sh
+++ money/get.sh
@@ -1,2 +1,2 @@
 firefox https://addons.mozilla.org/en-US/firefox/addon/chameleon-ext/ & sleep 2
-frefox https://addons.mozilla.org/en-US/firefox/addon/foxyproxy-standard/?src=search
+firefox https://addons.mozilla.org/en-US/firefox/addon/foxyproxy-standard/?src=search & sleep 2999999
