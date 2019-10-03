d144a70c620d187b995946a7ea4d123b5db43cd1
Mazbron <9880408+mazbron@users.noreply.github.com>
2019-10-03 15:10:06 +0700

    Update GD.sh

--- GD/GD.sh
+++ GD/GD.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 while :
 do
-
-firefox http://play.sarahvilo.id & sleep 3
+pkill --oldest firefoxfirefox http://play.sarahvilo.id & sleep 3
 echo START ACTIONA
 actiona -s -C -Q -e -x GD.ascr
-
 sleep 3
+pkill --oldest firefox
 apt-get clean
 sleep 5
 done
