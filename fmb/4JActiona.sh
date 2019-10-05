ce35f8606353dbc0121fa810cbe46c36c0bef6d1
Mazbron <9880408+mazbron@users.noreply.github.com>
2019-10-05 16:46:27 +0700

    Update 4JActiona.sh

--- fmb/4JActiona.sh
+++ fmb/4JActiona.sh
@@ -2,14 +2,25 @@
 printf '\033]2;helpbot\a'
 while :
 do
-proxychains chromium-browser --user-data-dir=/home/chrome --force-android-app-mode --app=http://halloween.wtf/4J.php --window-size=390,650 --window-position=1299,1 --no-sandbox --incognito & sleep 1
+do
+pkill --oldest chrome
+pkill --oldest chrome
+pkill --oldest chrome
+pkill --oldest chrome
+pkill --oldest chrome
+proxychains google-chrome --user-data-dir=/home/chrome60 --force-android-app-mode --app=http://halloween.wtf/4J.php --window-size=390,650 --window-position=1299,1 --no-sandbox --incognito & sleep 1
 clear
 sleep 17
 clear
 echo start helpbot
 actiona -s -C -Q -e -x 4j.ascr
-sleep 7
-killall chromium-browser
+sleep 5
+do
+pkill --oldest chrome
+pkill --oldest chrome
+pkill --oldest chrome
+pkill --oldest chrome
+pkill --oldest chrome
 apt-get autoclean
 clear
 sleep 3
