6df2f85b0e8d415b2e6876f9ca139a1b26c0063e
Mazbron <9880408+mazbron@users.noreply.github.com>
2019-10-12 17:42:11 +0000

    Add files via upload

---
+++ money/monetseoff.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+printf '\033]2;helpbot\a'
+while :
+do
+pkill --oldest firefox
+firefox & sleep 3
+firefox http://atozseotools.co/ & sleep 15
+firefox http://atozseotools.co/ & sleep 15
+firefox http://atozseotools.co/ & sleep 15
+pkill --oldest firefox
+sleep 5
+done
