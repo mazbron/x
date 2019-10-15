dd70571769d88875c55fbc3cd9c2ea55c1a353ef
Mazbron <9880408+mazbron@users.noreply.github.com>
2019-10-15 08:10:35 +0000

    Add files via upload

---
+++ fmb/4jsarah.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+printf '\033]2;helpbot\a'
+while :
+do
+pkill --oldest chrome
+ google-chrome --user-data-dir=/home/chrome60 https://sarahvilo.id/4j.html --no-sandbox & sleep 1
+clear
+sleep 75
+pkill --oldest chrome
+apt-get autoclean
+clear
+sleep 3
+/usr/games/sl -l
+sleep 1
+done
