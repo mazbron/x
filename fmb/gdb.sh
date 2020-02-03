ce04d1c23e41e702a3034f5bf4e0a55005ae83f4
Mazbron <9880408+mazbron@users.noreply.github.com>
2020-02-04 01:09:46 +0700

    Create gdb.sh

---
+++ fmb/gdb.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+printf '\033]2;helpbot\a'
+while :
+do
+pkill --oldest chrome
+ google-chrome --user-data-dir=/home/chrome60 https://gamesdb.co//4j.html --no-sandbox & sleep 1
+clear
+sleep 80
+pkill --oldest chrome
+apt-get autoclean
+clear
+sleep 3
+/usr/games/sl -l
+sleep 1
+done
