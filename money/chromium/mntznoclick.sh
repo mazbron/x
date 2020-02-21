b829f774a283bb8b992c05129770e472aa03a0ee
Mazbron <9880408+mazbron@users.noreply.github.com>
2020-02-22 02:13:35 +0700

    Update mntznoclick.sh

--- money/chromium/mntznoclick.sh
+++ money/chromium/mntznoclick.sh
@@ -5,12 +5,10 @@ pkill --oldest chromium
 pkill --oldest chromium
 pkill --oldest chromium
 chromium-browser --user-data-dir=/home/chrome http://atozgames.me --incognito --no-sandbox & sleep 8
-actiona -s -C -Q -e -x ffwindow2.ascr
+actiona -s -C -Q -e -x scroll.ascr
 pkill --oldest chromium
 pkill --oldest chromium
 pkill --oldest chromium
 apt-get autoclean
-rm -rf /root/.mozilla/firefox/szy8pis7.default-release/minidumps
-pkill --oldest firefox
 sleep 5
 done
