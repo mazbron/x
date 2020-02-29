d479a6fe412117b85f9b3da32d0d63a13e5557d5
Mazbron <9880408+mazbron@users.noreply.github.com>
2020-02-28 20:00:55 -0800

    Update mntzrclick.sh

--- money/chromium/mntzrclick.sh
+++ money/chromium/mntzrclick.sh
@@ -4,7 +4,8 @@ do
 pkill --oldest chromium
 pkill --oldest chromium
 pkill --oldest chromium
-chromium-browser --user-data-dir=/home/chrome http://l.facebook.com --incognito --no-sandbox & sleep 8
+chromium-browser --user-data-dir=/home/chrome  --incognito --no-sandbox & sleep 4
+chromium-browser --user-data-dir=/home/chrome http://l.facebook.com --incognito --no-sandbox & sleep 20
 actiona -s -C -Q -e -x mntzrclick.ascr
 pkill --oldest chromium
 pkill --oldest chromium
