12a2542fa1008d6a9f7e3299577ee88a107e4633
Mazbron <9880408+mazbron@users.noreply.github.com>
2020-03-11 20:08:54 +0700

    Update mntznoclick.sh

--- money/chromium/mntznoclick.sh
+++ money/chromium/mntznoclick.sh
@@ -4,6 +4,7 @@ do
 pkill --oldest chromium
 pkill --oldest chromium
 pkill --oldest chromium
+chromium-browser --user-data-dir=/home/chrome --incognito --no-sandbox & sleep 3
 chromium-browser --user-data-dir=/home/chrome http://l.facebook.com --incognito --no-sandbox & sleep 8
 actiona -s -C -Q -e -x scroll.ascr
 pkill --oldest chromium
