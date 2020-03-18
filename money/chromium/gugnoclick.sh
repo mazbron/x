d4c53fdf165f5482c1399385a0c342e06ef12647
Mazbron <9880408+mazbron@users.noreply.github.com>
2020-03-18 14:55:25 +0700

    Create gugnoclick.sh

---
+++ money/chromium/gugnoclick.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+while :
+do
+pkill --oldest chromium
+pkill --oldest chromium
+pkill --oldest chromium
+chromium-browser --user-data-dir=/home/chrome --incognito --no-sandbox & sleep 3
+chromium-browser --user-data-dir=/home/chrome https://t.co/fHTxED2UIV?amp=1 --incognito --no-sandbox & sleep 8
+actiona -s -C -Q -e -x scroll.ascr
+pkill --oldest chromium
+pkill --oldest chromium
+pkill --oldest chromium
+apt-get autoclean
+sleep 5
+done
