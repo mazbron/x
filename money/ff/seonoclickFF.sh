b91de9efa30fdd4a9d2973a694caaab8facca5e4
Mazbron <9880408+mazbron@users.noreply.github.com>
2020-03-20 03:14:31 +0700

    Update seonoclickFF.sh

--- money/ff/seonoclickFF.sh
+++ money/ff/seonoclickFF.sh
@@ -2,9 +2,9 @@
 while :
 do
 firefox & sleep 2
-firefox http://google.com & sleep 7
-firefox http://google.com & sleep 7
-firefox http://google.com & sleep 7
+firefox https://t.co/c2mF0GUeg9?amp=1 & sleep 7
+firefox https://t.co/c2mF0GUeg9?amp=1 & sleep 7
+firefox https://t.co/c2mF0GUeg9?amp=1 & sleep 7
 clear
 actiona -s -C -Q -e -x scroll2.ascr
 sleep 1
