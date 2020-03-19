c644950f1c7b8476caa9c9407521017ad3ba326b
Mazbron <9880408+mazbron@users.noreply.github.com>
2020-03-20 02:34:31 +0700

    Create seoFF.sh

---
+++ money/ff/seoFF.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+while :
+do
+firefox & sleep 2
+firefox http://google.com & sleep 27
+actiona -s -C -Q -e -x click.ascr
+clear
+apt-get autoclean
+sleep 1
+pkill --oldest firefox
+clear
+sleep 5
+done
