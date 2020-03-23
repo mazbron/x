258b70f0fd5dd29cdd94acaf313b4c057728ac82
Mazbron <9880408+mazbron@users.noreply.github.com>
2020-03-24 00:46:25 +0700

    Update rdp6gb.sh

--- rdp6gb.sh
+++ rdp6gb.sh
@@ -93,7 +93,7 @@ rm -rf .mozilla
 #wget https://github.com/mazbron/x/raw/master/money/ff/FF032020.tar.gz
 #tar -xvf FF032020.tar.gz
 wget https://github.com/mazbron/x/raw/master/money/ff/mzlnewUA.tar.gz
-mzlnewUA.tar.gz
+tar -xvf mzlnewUA.tar.gz
 mkdir Desktop
 cd Desktop
 #wget https://raw.githubusercontent.com/mazbron/x/master/ff.sh
