57452823d1873bfacc4bb268b26506100b5a6fd2
Mazbron <9880408+mazbron@users.noreply.github.com>
2020-04-17 20:42:51 +0000

    Add files via upload

---
+++ GD/sarahGD.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+printf '\033]2;HeaderLift\a'
+while :
+do
+clear
+echo scroll
+firefox -P p1 & sleep 4
+firefox -P p1 https://sarahvilo.id/action/virus-ninja-2/ & sleep 10
+actiona -s -C -Q -e -x GD.ascr
+#sleep 3
+#clear
+#firefox -P p2 & sleep 4
+#firefox -P p2 https://sarahvilo.id/action/virus-ninja-2/ & sleep 10
+#actiona -s -C -Q -e -x GD.ascr
+
+clear
+apt-get autoclean
+sleep 1
+
+sleep 5
+done
+
