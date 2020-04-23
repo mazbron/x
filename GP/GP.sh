402cb66cd4b2d136d2b9fb6e428fea166d578805
Mazbron <9880408+mazbron@users.noreply.github.com>
2020-04-23 16:18:31 +0000

    Add files via upload

---
+++ GP/GP.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+printf '\033]2;>helpbot_\a'
+while :
+do
+pkill --oldest chromium-browser
+pkill --oldest chromium-browser
+pkill --oldest chromium-browser
+pkill --oldest chromium-browser
+pkill --oldest chromium-browser
+chromium-browser --user-data-dir=/home/chrome --force-android-app-mode --app=http://play.sarahvilo.id/rope.html --window-size=460,700 --window-position=1,1 --no-sandbox --incognito & sleep 1
+clear
+sleep 10
+clear
+echo start helpbot
+actiona -s -C -Q -e -x GP.ascr
+sleep 3
+pkill --oldest chromium-browser
+pkill --oldest chromium-browser
+pkill --oldest chromium-browser
+pkill --oldest chromium-browser
+pkill --oldest chromium-browser
+apt-get autoclean
+clear
+sleep 1
+/usr/games/sl -l
+sleep 1
+done
