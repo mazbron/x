75af4d125c569719eed4f352e43d9195701a35aa
Mazbron <9880408+mazbron@users.noreply.github.com>
2020-04-23 23:36:56 +0700

    Update GP.sh

--- GP/GP.sh
+++ GP/GP.sh
@@ -2,11 +2,7 @@
 printf '\033]2;>helpbot_\a'
 while :
 do
-pkill --oldest chromium-browser
-pkill --oldest chromium-browser
-pkill --oldest chromium-browser
-pkill --oldest chromium-browser
-pkill --oldest chromium-browser
+pkill --oldest chromium
 chromium-browser --user-data-dir=/home/chrome --force-android-app-mode --app=http://play.sarahvilo.id/rope.html --window-size=460,700 --window-position=1,1 --no-sandbox --incognito & sleep 1
 clear
 sleep 10
@@ -14,11 +10,7 @@ clear
 echo start helpbot
 actiona -s -C -Q -e -x GP.ascr
 sleep 3
-pkill --oldest chromium-browser
-pkill --oldest chromium-browser
-pkill --oldest chromium-browser
-pkill --oldest chromium-browser
-pkill --oldest chromium-browser
+pkill --oldest chromium
 apt-get autoclean
 clear
 sleep 1
