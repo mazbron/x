6bafb529ab4aafaedeef3f7a26c601daa50bd1dc
Mazbron <9880408+mazbron@users.noreply.github.com>
2020-05-31 22:26:44 +0700

    Update gdb.sh

--- fmb/gdb.sh
+++ fmb/gdb.sh
@@ -3,13 +3,16 @@ printf '\033]2;helpbot\a'
 while :
 do
 pkill --oldest chrome
- google-chrome --user-data-dir=/home/chrome60 https://gamesdb.co//4j.html --no-sandbox & sleep 1
+ google-chrome --start-maximized --user-data-dir=/home/chrome60 https://gamesdb.co/4j.html --no-sandbox & sleep 1
 clear
-sleep 80
+sleep 75
 pkill --oldest chrome
 apt-get autoclean
 clear
-sleep 3
+sleep 2
+rm -rf /home/chrome60/Default/Cache/*
+rm /home/chrome60/Default/History 
+rm /home/chrome60/Default/Cookies
 /usr/games/sl -l
-sleep 1
+sleep 2
 done
