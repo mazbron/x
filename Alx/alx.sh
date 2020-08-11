deabc157d53e63a5c7ae1e8e22255c4757c57531
Mazbron <9880408+mazbron@users.noreply.github.com>
2020-08-11 23:56:31 +0700

    Update alx.sh

--- Alx/alx.sh
+++ Alx/alx.sh
@@ -4,7 +4,7 @@ do
 pkill --oldest chromium
 pkill --oldest chromium
 pkill --oldest chromium
-chromium-browser --user-data-dir=/home/alx mazbron.net/a --star-maximized --no-sandbox & sleep 5
+chromium-browser --user-data-dir=/home/alx mazbron.net/a.php --star-maximized --no-sandbox & sleep 5
 actiona -s -C -Q -e -x alx.ascr
 pkill --oldest chromium
 pkill --oldest chromium
