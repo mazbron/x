390f955bc8c5615a28de7aa30c5872696749dd09
Mazbron <9880408+mazbron@users.noreply.github.com>
2020-08-13 01:48:41 +0700

    Update sarahnoFF.sh

--- money/ff/sarahnoFF.sh
+++ money/ff/sarahnoFF.sh
@@ -8,7 +8,7 @@ firefox http://www.google.com & sleep 7
 clear
 actiona -s -C -Q -e -x scroll2.ascr
 sleep 1
-pkill --oldest firefox
+kill -9 $(ps -x | grep firefox)
 clear
 apt-get autoclean
 sleep 5
