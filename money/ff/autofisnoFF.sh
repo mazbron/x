6317224104d83efb06f14741d0963b4640a1ccb8
Mazbron <9880408+mazbron@users.noreply.github.com>
2020-08-13 01:48:21 +0700

    Update autofisnoFF.sh

--- money/ff/autofisnoFF.sh
+++ money/ff/autofisnoFF.sh
@@ -8,7 +8,7 @@ firefox http://autofis.com/go.php & sleep 7
 clear
 actiona -s -C -Q -e -x scroll2.ascr
 sleep 5
-pkill --oldest firefox
+kill -9 $(ps -x | grep firefox)
 clear
 apt-get autoclean
 sleep 3
