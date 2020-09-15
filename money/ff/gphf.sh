e82f2ba8ee684fb2e4d7292d89a2a442747ef8e7
Mazbron <9880408+mazbron@users.noreply.github.com>
2020-09-16 03:33:56 +0700

    Update gphf.sh

--- money/ff/gphf.sh
+++ money/ff/gphf.sh
@@ -2,7 +2,7 @@
 while :
 do
 firefox & sleep 2
-firefox https://atozgames.me/hf.html & sleep 5
+firefox file:///root/Desktop/gphf.html & sleep 5
 actiona -s -C -Q -e -x maxff.ascr
 clear
 apt-get autoclean
