6a96a4123c15649caa20210456519afe2cec73b1
Mazbron <9880408+mazbron@users.noreply.github.com>
2020-10-04 11:05:57 -0500

    Create gphf2.sh

---
+++ money/ff/gphf2.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+while :
+do
+
+firefox -p default-release -no-remote http://atozgames.me & sleep 15
+firefox -p 2 -no-remote http://atozgames.me & sleep 15
+firefox -p 3 -no-remote http://atozgames.me & sleep 15
+firefox -p 4 -no-remote http://atozgames.me & sleep 15
+firefox -p 5 -no-remote http://atozgames.me & sleep 15
+firefox -p default-release -no-remote http://atozgames.me & sleep 15
+firefox -p 2 -no-remote http://atozgames.me & sleep 15
+firefox -p 3 -no-remote http://atozgames.me & sleep 15
+firefox -p 4 -no-remote http://atozgames.me & sleep 15
+firefox -p 5 -no-remote http://atozgames.me & sleep 15
+firefox -p default-release -no-remote http://atozgames.me & sleep 15
+firefox -p 2 -no-remote http://atozgames.me & sleep 15
+firefox -p 3 -no-remote http://atozgames.me & sleep 15
+firefox -p 4 -no-remote http://atozgames.me & sleep 15
+firefox -p 5 -no-remote http://atozgames.me & sleep 15
+firefox -p default-release -no-remote http://atozgames.me & sleep 15
+firefox -p 2 -no-remote http://atozgames.me & sleep 15
+firefox -p 3 -no-remote http://atozgames.me & sleep 15
+firefox -p 4 -no-remote http://atozgames.me & sleep 15
+firefox -p 5 -no-remote http://atozgames.me & sleep 15
+firefox -p default-release -no-remote http://atozgames.me & sleep 15
+firefox -p 2 -no-remote http://atozgames.me & sleep 15
+firefox -p 3 -no-remote http://atozgames.me & sleep 15
+firefox -p 4 -no-remote http://atozgames.me & sleep 15
+firefox -p 5 -no-remote http://atozgames.me & sleep 15
+firefox -p default-release -no-remote http://atozgames.me & sleep 15
+firefox -p 2 -no-remote http://atozgames.me & sleep 15
+firefox -p 3 -no-remote http://atozgames.me & sleep 15
+firefox -p 4 -no-remote http://atozgames.me & sleep 15
+firefox -p 5 -no-remote http://atozgames.me & sleep 15
+firefox -p default-release -no-remote http://atozgames.me & sleep 15
+firefox -p 2 -no-remote http://atozgames.me & sleep 15
+firefox -p 3 -no-remote http://atozgames.me & sleep 15
+firefox -p 4 -no-remote http://atozgames.me & sleep 15
+firefox -p 5 -no-remote http://atozgames.me & sleep 15
+firefox -p default-release -no-remote http://atozgames.me & sleep 15
+firefox -p 2 -no-remote http://atozgames.me & sleep 15
+firefox -p 3 -no-remote http://atozgames.me & sleep 15
+firefox -p 4 -no-remote http://atozgames.me & sleep 15
+firefox -p 5 -no-remote http://atozgames.me & sleep 15
+firefox -p default-release -no-remote http://atozgames.me & sleep 15
+firefox -p 2 -no-remote http://atozgames.me & sleep 15
+firefox -p 3 -no-remote http://atozgames.me & sleep 15
+firefox -p 4 -no-remote http://atozgames.me & sleep 15
+firefox -p 5 -no-remote http://atozgames.me & sleep 15
+firefox -p default-release -no-remote http://atozgames.me & sleep 15
+firefox -p 2 -no-remote http://atozgames.me & sleep 15
+firefox -p 3 -no-remote http://atozgames.me & sleep 15
+firefox -p 4 -no-remote http://atozgames.me & sleep 15
+firefox -p 5 -no-remote http://atozgames.me & sleep 15
+firefox -p default-release -no-remote http://atozgames.me & sleep 15
+firefox -p 2 -no-remote http://atozgames.me & sleep 15
+firefox -p 3 -no-remote http://atozgames.me & sleep 15
+firefox -p 4 -no-remote http://atozgames.me & sleep 15
+firefox -p 5 -no-remote http://atozgames.me & sleep 15
+firefox -p default-release -no-remote http://atozgames.me & sleep 15
+firefox -p 2 -no-remote http://atozgames.me & sleep 15
+firefox -p 3 -no-remote http://atozgames.me & sleep 15
+firefox -p 4 -no-remote http://atozgames.me & sleep 15
+firefox -p 5 -no-remote http://atozgames.me & sleep 17
+
+clear
+sleep 7
+#actiona -s -C -Q -e -x scroll2.ascr
+kill -9 $(ps -x | grep firefox)
+clear
+apt-get autoclean
+sleep 3
+done
