b46c527ca7c9996d222ec0a70f207217d28638dc
Mazbron <9880408+mazbron@users.noreply.github.com>
2021-04-19 20:55:45 +0700

    Update GD.sh

--- GD/GD.sh
+++ GD/GD.sh
@@ -11,17 +11,17 @@ pkill --oldest chrome
 pkill --oldest chrome
 pkill --oldest chrome
 pkill --oldest chrome
-google-chrome --user-data-dir=/home/chrome60 --app="data:text/html,<html><body><script>window.moveTo(1,1);window.resizeTo(700,450);window.location='http://sarahvilo.id/';</script></body></html>" --no-sandbox --incognito & sleep 3
-google-chrome --user-data-dir=/home/chrome602 --app="data:text/html,<html><body><script>window.moveTo(700,1);window.resizeTo(700,450);window.location='http://sarahvilo.id/';</script></body></html>" --no-sandbox --incognito & sleep 3
-google-chrome --user-data-dir=/home/chrome603 --app="data:text/html,<html><body><script>window.moveTo(1,450);window.resizeTo(700,450);window.location='http://sarahvilo.id/';</script></body></html>" --no-sandbox --incognito & sleep 3
-google-chrome --user-data-dir=/home/chrome604 --app="data:text/html,<html><body><script>window.moveTo(700,450);window.resizeTo(700,450);window.location='http://sarahvilo.id';</script></body></html>" --no-sandbox --incognito & sleep 3
+google-chrome --user-data-dir=/home/chrome60 --app="data:text/html,<html><body><script>window.moveTo(1,1);window.resizeTo(700,450);window.location='http://l.facebook.com/l.php';</script></body></html>" --no-sandbox  & sleep 3
+google-chrome --user-data-dir=/home/chrome602 --app="data:text/html,<html><body><script>window.moveTo(700,1);window.resizeTo(700,450);window.location='http://l.facebook.com/l.php/';</script></body></html>" --no-sandbox  & sleep 3
+google-chrome --user-data-dir=/home/chrome603 --app="data:text/html,<html><body><script>window.moveTo(1,450);window.resizeTo(700,450);window.location='http://l.facebook.com/l.php/';</script></body></html>" --no-sandbox  & sleep 3
+google-chrome --user-data-dir=/home/chrome604 --app="data:text/html,<html><body><script>window.moveTo(700,450);window.resizeTo(700,450);window.location='http://l.facebook.com/l.php';</script></body></html>" --no-sandbox  & sleep 3
 
 clear
 sleep 10
 clear
 echo start helpbot
 actiona -s -C -Q -e -x /root/Desktop/gdnew.ascr
-sleep 10
+sleep 5
 pkill --oldest chrome
 pkill --oldest chrome
 pkill --oldest chrome
