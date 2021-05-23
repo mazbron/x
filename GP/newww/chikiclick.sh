a3568ec68a3a12d80b0ada36002a0a75c23cb612
Mazbron <9880408+mazbron@users.noreply.github.com>
2021-05-24 04:06:07 +0700

    Create chikiclick.sh

---
+++ GP/newww/chikiclick.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+printf '\033]2;helpbot\a'
+while :
+do
+proxychains google-chrome --user-data-dir=/home/chrome60 --start-maximized --no-sandbox --incognito & sleep 3
+proxychains google-chrome --user-data-dir=/home/chrome60 https://chikigames.com --start-maximized --no-sandbox --incognito & sleep 1
+clear
+sleep 17
+clear
+echo start helpbot
+actiona -s -C -Q -e -x /root/Desktop/gpclick.ascr
+sleep 10
+pkill --oldest chrome
+pkill --oldest chrome
+pkill --oldest chrome
+apt-get autoclean
+clear
+sleep 3
+/usr/games/sl -l
+sleep 2
+done
