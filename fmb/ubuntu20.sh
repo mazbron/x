a84471bf04874e01697c7ff2e571346306f89737
Mazbron <9880408+mazbron@users.noreply.github.com>
2022-05-05 21:06:56 +0700

    Update ubuntu20.sh

--- fmb/ubuntu20.sh
+++ fmb/ubuntu20.sh
@@ -249,10 +249,11 @@ apt-get install sl -y
 #sl -l
 snap install termdown
 cd /etc
-mv hosts hosts.bak
+mv hosts hosts.bakk
 mv proxychains.conf proxychains.conf.bak
 wget https://github.com/mazbron/x/raw/master/fmb/proxychains.conf
 wget https://github.com/mazbron/x/raw/master/fmb/hosts
+cp hosts hosts.bak
 echo "root:gl0d4kgl0d4k" | sudo chpasswd
 sudo service xrdp restart
 nano /etc/hosts
