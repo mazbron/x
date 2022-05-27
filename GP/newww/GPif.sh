296ffab369bb067658235597e69015d4fce2a582
Mazbron <9880408+mazbron@users.noreply.github.com>
2022-05-27 20:10:40 +0700

    Update GPif.sh

--- GP/newww/GPif.sh
+++ GP/newww/GPif.sh
@@ -3,7 +3,7 @@ printf '\033]2;helpbot\a'
 while :
 do
  google-chrome --user-data-dir=/home/chrome60 --start-maximized --no-sandbox --incognito & sleep 3
- google-chrome --user-data-dir=/home/chrome60 https://sarahvilo.id --start-maximized --no-sandbox --incognito & sleep 1
+ google-chrome --user-data-dir=/home/chrome60 https://mazbron.net/gp.php --start-maximized --no-sandbox --incognito & sleep 1
 clear
 sleep 10
 clear
