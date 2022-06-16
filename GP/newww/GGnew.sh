a62913f28f2743c84a136e7c9ae98812b794c435
Mazbron <9880408+mazbron@users.noreply.github.com>
2022-06-16 14:00:51 +0700

    Create GGnew.sh

---
+++ GP/newww/GGnew.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+printf '\033]2;helpbot\a'
+while :
+do
+rm -rf /home/chrome60/Default/Cache/*
+sleep 2
+ google-chrome --user-data-dir=/home/chrome60 --start-maximized --no-sandbox --incognito & sleep 3
+ google-chrome --user-data-dir=/home/chrome60  gimgiman.com --start-maximized --no-sandbox --incognito & sleep 1
+clear
+sleep 10
+clear
+echo start helpbot
+actiona -s -C -Q -e -x /root/Desktop/gp.ascr
+sleep 50
+pkill --oldest chrome
+pkill --oldest chrome
+pkill --oldest chrome
+apt-get autoclean
+clear
+sleep 3
+/usr/games/sl -l
+sleep 2
+done
