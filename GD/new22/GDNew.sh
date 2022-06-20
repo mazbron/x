40b7c1906395903d1d24a515ed7ac436b90f1dcb
Mazbron <9880408+mazbron@users.noreply.github.com>
2022-06-20 20:55:00 +0700

    Update GDNew.sh

--- GD/new22/GDNew.sh
+++ GD/new22/GDNew.sh
@@ -2,6 +2,8 @@
 printf '\033]2;helpbot\a'
 while :
 do
+rm -rf /home/chrome60/Default/Cache/*
+sleep 2
  google-chrome --user-data-dir=/home/chrome60 --start-maximized --no-sandbox --incognito & sleep 5
  google-chrome --user-data-dir=/home/chrome60 http://WWW.GOOGLE.COM --start-maximized --no-sandbox --incognito & sleep 1
 clear
