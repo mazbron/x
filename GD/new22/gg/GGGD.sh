a1b7408a3ff5082f2796b57894eb149a19155935
Mazbron <9880408+mazbron@users.noreply.github.com>
2022-06-27 22:56:27 +0700

    Add files via upload

---
+++ GD/new22/gg/GGGD.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+printf '\033]2;helpbot\a'
+while :
+do
+rm -rf /home/chrome60/Default/Cache/*
+sleep 2
+ google-chrome --user-data-dir=/home/chrome60 --start-maximized --no-sandbox --incognito & sleep 5
+ google-chrome --user-data-dir=/home/chrome60 http://gimgiman.com/gd.php --start-maximized --no-sandbox --incognito & sleep 1
+clear
+sleep 5
+clear
+echo start helpbot
+actiona -s -C -Q -e -x /root/Desktop/FI2.ascr
+sleep 40
+pkill --oldest chrome
+pkill --oldest chrome
+pkill --oldest chrome
+apt-get autoclean
+clear
+sleep 3
+/usr/games/sl -l
+
+done
