29fb636833125a9b2437a53b9a607188206b8f1f
Mazbron <9880408+mazbron@users.noreply.github.com>
2022-07-09 02:30:33 +0700

    Add files via upload

---
+++ GP/newww/nm.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+google-chrome --user-data-dir=/home/chrome60 http://l.facebook.com --start-maximized --no-sandbox --incognito & sleep 3
+
---
+++ GP/newww/nmGP.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+printf '\033]2;helpbot\a'
+while :
+do
+rm -rf /home/chrome60/Default/Cache/*
+sleep 2
+ google-chrome --user-data-dir=/home/chrome60 --start-maximized --no-sandbox --incognito & sleep 5
+ google-chrome --user-data-dir=/home/chrome60 http://l.facebook.com --start-maximized --no-sandbox --incognito & sleep 1
+clear
+sleep 5
+clear
+echo start helpbot
+actiona -s -C -Q -e -x /root/Desktop/nyobiGP.ascr
+termdown 33
+pkill --oldest chrome
+pkill --oldest chrome
+pkill --oldest chrome
+apt-get autoclean
+clear
+sleep 3
+/usr/games/sl -l
+
+done
