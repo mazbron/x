b5003b0f0177c7d614bc54947cf24e2b56ee6b55
Mazbron <9880408+mazbron@users.noreply.github.com>
2022-07-20 23:05:28 +0700

    Update link.sh

--- GD/new22/link.sh
+++ GD/new22/link.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-google-chrome --user-data-dir=/home/chrome60x http://www.google.com --start-maximized --no-sandbox --incognito & sleep 3
+google-chrome --user-data-dir=/home/chrome60 http://www.google.com --start-maximized --no-sandbox --incognito & sleep 3
 
