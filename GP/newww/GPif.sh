df7357a03be1072e89efeb78cefea690bf5a5195
Mazbron <9880408+mazbron@users.noreply.github.com>
2022-11-09 23:52:32 +0700

    Update GPif.sh

--- GP/newww/GPif.sh
+++ GP/newww/GPif.sh
@@ -3,9 +3,10 @@ printf '\033]2;helpbot\a'
 while :
 do
 rm -rf /home/chrome60/Default/Cache/*
+rm -rf /root/Downloads/*
 sleep 2
  google-chrome --user-data-dir=/home/chrome60 --start-maximized --no-sandbox --incognito & sleep 3
- google-chrome --user-data-dir=/home/chrome60 https://mazbron.net/gp.php --start-maximized --no-sandbox --incognito & sleep 1
+ google-chrome --user-data-dir=/home/chrome60 https://mazbron.net/gpakun1.php --start-maximized --no-sandbox --incognito & sleep 1
 clear
 termdown 10
 clear
