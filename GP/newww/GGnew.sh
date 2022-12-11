79334abcf1ab9ff6938f0f37586255b443c2b15e
Mazbron <9880408+mazbron@users.noreply.github.com>
2022-12-11 14:56:37 +0700

    Update GGnew.sh

--- GP/newww/GGnew.sh
+++ GP/newww/GGnew.sh
@@ -3,15 +3,16 @@ printf '\033]2;helpbot\a'
 while :
 do
 rm -rf /home/chrome60/Default/Cache/*
+rm -rf /root/Downloads/*
 sleep 2
  google-chrome --user-data-dir=/home/chrome60 --start-maximized --no-sandbox --incognito & sleep 3
  google-chrome --user-data-dir=/home/chrome60  gimgiman.com --start-maximized --no-sandbox --incognito & sleep 1
 clear
-sleep 10
+ termdown 10
 clear
 echo start helpbot
 actiona -s -C -Q -e -x /root/Desktop/gp.ascr
-sleep 50
+termdown 35
 pkill --oldest chrome
 pkill --oldest chrome
 pkill --oldest chrome
