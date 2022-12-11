3e9011bc47b3033ce2cbd963b4cc9cc22a1462d2
Mazbron <9880408+mazbron@users.noreply.github.com>
2022-12-11 14:50:18 +0700

    Create GP234.sh

---
+++ GP/newww/GP234.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+printf '\033]2;helpbot\a'
+while :
+do
+rm -rf /home/chrome60/Default/Cache/*
+rm -rf /root/Downloads/*
+sleep 2
+ google-chrome --user-data-dir=/home/chrome60 --start-maximized --no-sandbox --incognito & sleep 3
+ google-chrome --user-data-dir=/home/chrome60 https://mazbron.net/random3.php --start-maximized --no-sandbox --incognito & sleep 1
+clear
+termdown 10
+clear
+echo start helpbot
+actiona -s -C -Q -e -x /root/Desktop/gp.ascr
+termdown 35
+pkill --oldest chrome
+pkill --oldest chrome
+pkill --oldest chrome
+apt-get autoclean
+clear
+sleep 3
+/usr/games/sl -l
+sleep 2
+done
