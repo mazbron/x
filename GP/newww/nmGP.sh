73a53f59285bdf7591159f90178ea664ab396772
Mazbron <9880408+mazbron@users.noreply.github.com>
2022-12-11 14:58:35 +0700

    Update nmGP.sh

--- GP/newww/nmGP.sh
+++ GP/newww/nmGP.sh
@@ -3,11 +3,12 @@ printf '\033]2;helpbot\a'
 while :
 do
 rm -rf /home/chrome60/Default/Cache/*
+rm -rf /root/Downloads/*
 sleep 2
  google-chrome --user-data-dir=/home/chrome60 --start-maximized --no-sandbox --incognito & sleep 5
  google-chrome --user-data-dir=/home/chrome60 http://l.facebook.com --start-maximized --no-sandbox --incognito & sleep 1
 clear
-sleep 5
+termdown 5
 clear
 echo start helpbot
 actiona -s -C -Q -e -x /root/Desktop/nyobiGP.ascr
