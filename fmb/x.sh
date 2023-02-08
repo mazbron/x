0c7aee37a8d60fb2ec554b777a598cae397c32b2
Mazbron <9880408+mazbron@users.noreply.github.com>
2023-02-08 18:54:48 +0700

    Update x.sh

--- fmb/x.sh
+++ fmb/x.sh
@@ -1,27 +1,11 @@
-#!/bin/bash
-printf '\033]2;>helpbot_\a'
-while :
-do
-pkill --oldest chrome
-pkill --oldest chrome
-pkill --oldest chrome
-pkill --oldest chrome
-pkill --oldest chrome
-proxychains chromium-browser --user-data-dir=/home/chrome60 --force-android-app-mode --app=http://halloween.wtf/4J.php --window-size=390,650 --window-position=1299,1 --no-sandbox --incognito & sleep 1
-clear
-sleep 17
-clear
-echo start helpbot
-actiona -s -C -Q -e -x /root/Desktop/lib/4j.ascr
-sleep 7
-pkill --oldest chrome
-pkill --oldest chrome
-pkill --oldest chrome
-pkill --oldest chrome
-pkill --oldest chrome
-apt-get autoclean
-clear
+sudo apt install lxde -y 
+#new ubuntu 20.04
+apt install net-tools -y
 sleep 1
-/usr/games/sl -l
-sleep 1
-done
+#end new
+echo Removing Screensaver
+apt-get remove xscreensaver -y
+sudo service xrdp restart
+nano /etc/hosts
+sudo systemctl enable xrdp
+sudo update-alternatives --config x-session-manager
