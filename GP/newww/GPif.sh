aeb11e673e5bdf245ec1db58a84a1a01b748c13e
Mazbron <9880408+mazbron@users.noreply.github.com>
2023-05-20 19:21:05 +0700

    Update GPif.sh

--- GP/newww/GPif.sh
+++ GP/newww/GPif.sh
@@ -8,11 +8,11 @@ sleep 2
  google-chrome --user-data-dir=/home/chrome60 --start-maximized --no-sandbox --incognito & sleep 3
  google-chrome --user-data-dir=/home/chrome60 https://mazbron.net/gpakun1.php --start-maximized --no-sandbox --incognito & sleep 1
 clear
-termdown 10
+sleep 10
 clear
 echo start helpbot
 actiona -s -C -Q -e -x /root/Desktop/gp.ascr
-termdown 35
+sleep 35
 pkill --oldest chrome
 pkill --oldest chrome
 pkill --oldest chrome
