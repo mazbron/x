fdee7cfad02e7d870ac5815ee56dfe4888971107
Mazbron <9880408+mazbron@users.noreply.github.com>
2023-06-22 23:19:33 +0700

    Update GPif.sh

--- GP/newww/GPif.sh
+++ GP/newww/GPif.sh
@@ -4,21 +4,22 @@ while :
 do
 rm -rf /home/chrome60/Default/Cache/*
 rm -rf /root/Downloads/*
-sleep 2
+rm -rf  /home/chrome60/BrowserMetrics
+termdown 2
  google-chrome --user-data-dir=/home/chrome60 --start-maximized --no-sandbox --incognito & sleep 3
  google-chrome --user-data-dir=/home/chrome60 https://mazbron.net/gpakun1.php --start-maximized --no-sandbox --incognito & sleep 1
 clear
-sleep 10
+termdown 10
 clear
 echo start helpbot
 actiona -s -C -Q -e -x /root/Desktop/gp.ascr
-sleep 35
+termdown 35
 pkill --oldest chrome
 pkill --oldest chrome
 pkill --oldest chrome
 apt-get autoclean
 clear
-sleep 3
+termdown 3
 /usr/games/sl -l
-sleep 2
+termdown 2
 done
