968cb2919aab79fe44e94ab07bcf12d5bb24a44d
Mazbron <9880408+mazbron@users.noreply.github.com>
2023-06-22 23:19:11 +0700

    Update GPclick.sh

--- GP/newww/GPclick.sh
+++ GP/newww/GPclick.sh
@@ -3,21 +3,23 @@ printf '\033]2;helpbot\a'
 while :
 do
 rm -rf /home/chrome60/Default/Cache/*
-sleep 2
+rm -rf /root/Downloads/*
+rm -rf  /home/chrome60/BrowserMetrics
+termdown 2
  google-chrome --user-data-dir=/home/chrome60 --start-maximized --no-sandbox --incognito & sleep 3
  google-chrome --user-data-dir=/home/chrome60 https://mazbron.net/gp.php --start-maximized --no-sandbox --incognito & sleep 1
 clear
-sleep 17
+termdown 17
 clear
 echo start helpbot
 actiona -s -C -Q -e -x /root/Desktop/gpclick.ascr
-sleep 10
+termdown 10
 pkill --oldest chrome
 pkill --oldest chrome
 pkill --oldest chrome
 apt-get autoclean
 clear
-sleep 3
+termdown 3
 /usr/games/sl -l
-sleep 2
+termdown 2
 done
