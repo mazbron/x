ae71836efe2fd55fd5e7d6d031f99b90f1d9c2a3
Mazbron <9880408+mazbron@users.noreply.github.com>
2023-09-18 22:25:28 +0700

    Update eka.sh

--- eka.sh
+++ eka.sh
@@ -1,21 +1,30 @@
-#!/bin/bash
-while :
-do
-google-chrome --user-data-dir=/home/chrome --app="data:text/html,<html><body><script>window.moveTo(1,1);window.resizeTo(300,200);window.location='https://t.co/jgbIIRljvt';</script></body></html>" --no-sandbox --disable-session--crashed-bubble --incognito & sleep 3
-google-chrome --user-data-dir=/home/chrome --app="data:text/html,<html><body><script>window.moveTo(1,220);window.resizeTo(300,200);window.location='https://t.co/jgbIIRljvt';</script></body></html>" --no-sandbox --disable-session--crashed-bubble --incognito & sleep 3
-google-chrome --user-data-dir=/home/chrome --app="data:text/html,<html><body><script>window.moveTo(1,440);window.resizeTo(300,200);window.location='https://t.co/jgbIIRljvt';</script></body></html>" --no-sandbox --disable-session--crashed-bubble --incognito & sleep 5
-google-chrome --user-data-dir=/home/chrome --app="data:text/html,<html><body><script>window.moveTo(310,1);window.resizeTo(300,200);window.location='https://t.co/jgbIIRljvt';</script></body></html>" --no-sandbox --disable-session--crashed-bubble --incognito & sleep 3
-google-chrome --user-data-dir=/home/chrome --app="data:text/html,<html><body><script>window.moveTo(620,1);window.resizeTo(300,200);window.location='https://t.co/jgbIIRljvt';</script></body></html>" --no-sandbox --disable-session--crashed-bubble --incognito & sleep 3
-google-chrome --user-data-dir=/home/chrome --app="data:text/html,<html><body><script>window.moveTo(930,1);window.resizeTo(300,200);window.location='https://t.co/jgbIIRljvt';</script></body></html>" --no-sandbox --disable-session--crashed-bubble --incognito & sleep 5
-google-chrome --user-data-dir=/home/chrome --app="data:text/html,<html><body><script>window.moveTo(930,220);window.resizeTo(300,200);window.location='https://t.co/jgbIIRljvt';</script></body></html>" --no-sandbox --disable-session--crashed-bubble --incognito & sleep 3
-google-chrome --user-data-dir=/home/chrome --app="data:text/html,<html><body><script>window.moveTo(620,220);window.resizeTo(300,200);window.location='https://t.co/jgbIIRljvt';</script></body></html>" --no-sandbox --disable-session--crashed-bubble --incognito & sleep 3
-google-chrome --user-data-dir=/home/chrome --app="data:text/html,<html><body><script>window.moveTo(310,220);window.resizeTo(300,200);window.location='https://t.co/jgbIIRljvt';</script></body></html>" --no-sandbox --disable-session--crashed-bubble --incognito & sleep 5
-google-chrome --user-data-dir=/home/chrome --app="data:text/html,<html><body><script>window.moveTo(930,440);window.resizeTo(300,200);window.location='https://t.co/jgbIIRljvt';</script></body></html>" --no-sandbox --disable-session--crashed-bubble --incognito & sleep 3
-google-chrome --user-data-dir=/home/chrome --app="data:text/html,<html><body><script>window.moveTo(620,440);window.resizeTo(300,200);window.location='https://t.co/jgbIIRljvt';</script></body></html>" --no-sandbox --disable-session--crashed-bubble --incognito & sleep 3
-google-chrome --user-data-dir=/home/chrome --app="data:text/html,<html><body><script>window.moveTo(310,440);window.resizeTo(300,200);window.location='https://t.co/jgbIIRljvt';</script></body></html>" --no-sandbox --disable-session--crashed-bubble --incognito & sleep 200
-killall chrome
-killall chrome
-killall chrome
-apt-get clean
-sleep 5
-done
+sudo apt install xrdp -y 
+sleep 1
+sudo adduser xrdp ssl-cert
+sleep 1
+sudo systemctl restart xrdp
+sleep 1
+sudo ufw allow 3389
+sudo ufw allow from 1.1.1.1 to any port 3389
+sleep 1
+sudo apt install lxde -y 
+#new ubuntu 20.04
+apt install net-tools -y
+sleep 1
+#end new
+echo Removing Screensaver
+apt-get remove xscreensaver -y
+sudo service xrdp restart
+sleep 1
+echo hapus file ga penting
+rm /root/ff19ipflood.tar.gz /root/libflashplayer.so /root/install_flash_player_11_linux.x86_64.tar.gz
+rm -rf /home/de /home/nl /home/chrome60x
+rm /home/chprof2022.zip
+sleep 1
+nano /etc/hosts
+sudo systemctl enable xrdp
+sudo update-alternatives --config x-session-manager
+
+echo You have successfully Installed LXDE Desktop Environment.. Enjoy..!!
+echo Ceeeers... MAZBRON.com _ BESTSEOTOOL.co
+echo Juni 2023
