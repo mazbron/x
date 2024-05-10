b14ad0c99c213627e21140f6d6fdc95a5a4204c8
Mazbron <9880408+mazbron@users.noreply.github.com>
2024-05-10 22:52:30 +0700

    Create GP3.sh

---
+++ GP/newww/GP3.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+printf '\033]2;GP3\a'
+while :
+do
+rm -rf /home/chrome60/Default/Cache/*
+find /root/Downloads -type f -name '.*' -exec rm {} +
+rm -rf  /home/chrome60/BrowserMetrics
+termdown 2
+proxychains google-chrome --user-data-dir=/home/chrome60 file:///root/Desktop/close.html --start-maximized --no-sandbox --incognito & sleep 5
+ google-chrome --user-data-dir=/home/chrome60 https://mazbron.net/gpakun3.php --start-maximized --no-sandbox --incognito & sleep 10
+ google-chrome --user-data-dir=/home/chrome60 https://mazbron.net/gpakun3.php --start-maximized --no-sandbox --incognito & sleep 5
+clear
+termdown 1
+clear
+echo start helpbot
+actiona -s -C -Q -e -x /root/Desktop/gp.ascr
+termdown 35
+actiona -s -C -Q -e -x /root/Desktop/killchrome.ascr
+pkill --oldest chrome
+pkill --oldest chrome
+pkill --oldest chrome
+apt-get autoclean
+clear
+termdown 3
+/usr/games/sl -l
+termdown 5
+done
