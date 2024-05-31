7a9985c30282339901a464b8353ffc3c34b55065
Mazbron <9880408+mazbron@users.noreply.github.com>
2024-06-01 00:14:19 +0700

    Create GP33.sh

---
+++ GP/newww/GP33.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+printf '\033]2;helpbot\a'
+while :
+do
+rm -rf /home/chrome60/Default/Cache/*
+find /root/Downloads -type f -name '.*' -exec rm {} +
+rm -rf  /home/chrome60/BrowserMetrics
+termdown 2
+ google-chrome --user-data-dir=/home/chrome60 https://mazbron.net/close.html --start-maximized --no-sandbox --incognito & sleep 5
+ google-chrome --user-data-dir=/home/chrome60 https://mazbron.net/gpakun3.php --start-maximized --no-sandbox --incognito & sleep 1
+clear
+termdown 10
+clear
+echo start helpbot
+actiona -s -C -Q -e -x /root/Desktop/gp.ascr
+termdown 35
+pkill --oldest chrome
+pkill --oldest chrome
+pkill --oldest chrome
+apt-get autoclean
+clear
+termdown 3
+/usr/games/sl -l
+termdown 5
+done
